f1788fa529cf2591322f016d29302d7ac9297aa6
koppresh <koppresh.putpak@agilepoint.com>
2023-09-18 19:10:38 +0530

    feat: Added styled components and react router libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import './App.css'
+import styled from "styled-components"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { About, Home } from "./modules";
+
+const StyledH1 = styled.h1`
+  font-size: 24px;
+  color: red;
+`;
 
 export default function App() {
   return (
-    <>
-      <h1>This is the dev Branch</h1>
-    </>
+    <BrowserRouter>
+      <StyledH1>This is the dev Branch</StyledH1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ src/modules/about.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const About = React.memo(() => {
+    return (
+        <>
+            <div>About pAge</div>
+        </>
+    )
+})
