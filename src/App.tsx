ed0ecb72706b5efb22cbf2dc7b324dd588ead5f2
koppresh <koppresh.putpak@agilepoint.com>
2023-09-19 17:32:19 +0530

    fix: Added redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import styled from "styled-components"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { About, Home } from "./modules";
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+
+const store = configureStore({
+  reducer: {},
+})
 
 const StyledH1 = styled.h1`
   font-size: 24px;
-  color: red;
+  color: #d82d2d;
 `;
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <StyledH1>This is the dev Branch</StyledH1>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <StyledH1>This is the dev Branch</StyledH1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   )
 }
