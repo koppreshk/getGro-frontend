db83c148576f7d34f073c682451a2ddc01028b17
Siddarth Menon <siddarth.menon@agilepoint.com>
2023-09-21 15:46:14 +0530

    feat: add material UI lib support

--- src/modules/about.tsx
+++ src/modules/about.tsx
@@ -1,9 +1,15 @@
+import { Button } from "@mui/material"
 import React from "react"
 
 export const About = React.memo(() => {
     return (
         <>
             <div>About pAge</div>
+            <ButtonUsage/>
         </>
     )
 })
+
+const ButtonUsage = () => {
+    return <Button variant="outlined">Hello World</Button>
+}
