47ec790401648327597a4b80387fbae745262386
koppresh <koppresh.putpak@agilepoint.com>
2023-09-28 17:42:52 +0530

    fix: Added css fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 import { CoreLayoutPage } from "./modules/core/pages/core-layout-page";
 import { ThemeProvider, defaultMUITheme } from "themes";
 import { ThemeProvider as MUIthemeProvider } from "@mui/material";
 import { NotificationProvider } from "lib";
-import { DataGrid, FlexBox, columns, defaultData } from "lib/ui-ux";
 
 const store = configureStore({
   reducer: {},
@@ -18,12 +17,7 @@ export default function App() {
         <NotificationProvider>
           <BrowserRouter>
             <ThemeProvider>
-              <FlexBox $width="100%" $height="100%">
-                <CoreLayoutPage />
-                <Routes>
-                  <Route element={<DataGrid columns={columns} data={defaultData} />} key="route1" path="/route1" />
-                </Routes>
-              </FlexBox>
+              <CoreLayoutPage />
             </ThemeProvider>
           </BrowserRouter>
         </NotificationProvider>
--- src/modules/core/pages/core-layout-page.tsx
+++ src/modules/core/pages/core-layout-page.tsx
@@ -1,12 +1,19 @@
 import React from "react"
 import { NavigationMenu } from "../components"
 import { TopNavigationMenu } from "../components/top-navigation-menu"
+import { DataGrid, FlexBox, columns, defaultData } from "lib/ui-ux"
+import { Routes, Route } from "react-router-dom"
 
 export const CoreLayoutPage = React.memo(() => {
     return (
         <>
             <TopNavigationMenu />
-            <NavigationMenu />
+            <FlexBox $width="100%" $height="100%">
+                <NavigationMenu />
+                <Routes>
+                    <Route element={<DataGrid columns={columns} data={defaultData} />} key="route1" path="/route1" />
+                </Routes>
+            </FlexBox>
         </>
     )
 })
