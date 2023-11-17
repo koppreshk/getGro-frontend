730ef265f3c581f5de3db99842e53e96b96a5935
koppresh <koppresh.putpak@agilepoint.com>
2023-11-17 15:56:51 +0530

    fix: Added form hook validation support

--- src/lib/form-fields/textbox-field.tsx
+++ src/lib/form-fields/textbox-field.tsx
@@ -1,19 +1,34 @@
 import { TextField, TextFieldProps } from "@mui/material"
-import { Controller, useFormContext } from "react-hook-form"
+import { Controller, FieldValues, RegisterOptions, useFormContext } from "react-hook-form"
+import styled from "styled-components";
+import { ErrorMessage } from '@hookform/error-message';
 
-type ITextboxFieldProps = TextFieldProps & {
+type ITextboxFieldProps = Omit<TextFieldProps, 'error' | 'required'> & {
     name: string;
+    rules?: Omit<RegisterOptions<FieldValues, string>, "disabled" | "valueAsNumber" | "valueAsDate" | "setValueAs"> | undefined
 }
 
+const StyledErrorMessage = styled.div`
+  &&{
+    color: #d32f2f;
+    font-size: 13px;
+  }  
+`;
+
 export const TextboxField = (props: ITextboxFieldProps) => {
-    const { name } = props;
-    const { control } = useFormContext();
+    const { name, rules } = props;
+    const { formState: { errors }, control } = useFormContext();
+    const hasError = name in errors;
 
     return (
-        <Controller
-            render={({ field }) => <TextField {...props} {...field} />}
-            control={control}
-            name={name}
-        />
+        <>
+            <Controller
+                render={({ field }) => <TextField {...props} {...field} error={hasError} />}
+                control={control}
+                name={name}
+                rules={rules}
+            />
+            <ErrorMessage errors={errors} name={name} as={StyledErrorMessage} />
+        </>
     )
 } 
