570390c67619eeeadfa63cbf23de50df2fb5067a
koppresh <koppresh.putpak@agilepoint.com>
2023-12-13 16:58:37 +0530

    fix: added box shadow

--- src/modules/tickets/components/ticket-details/ticket-conversation/email-conversations/email-card.tsx
+++ src/modules/tickets/components/ticket-details/ticket-conversation/email-conversations/email-card.tsx
@@ -36,6 +36,10 @@ const EditorContainer = styled(FlexBox)`
         display: flex;
         flex-direction: column;
     }
+    .quill:hover, :focus {
+            border-radius: 16px;
+            box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+    }
     .ql-toolbar {
         border-radius: 16px 16px 0px 0px;
     }
