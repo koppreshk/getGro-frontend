c938e8902fd26b7e3e58fb4665c302b731da30c2
koppresh <koppresh.putpak@agilepoint.com>
2023-12-15 13:01:11 +0530

    fix: Added popover state

---
+++ src/modules/tickets/components/ticket-details/ticket-conversation/email-conversations/editor-section.tsx
@@ -0,0 +1,60 @@
+import { Send, Delete } from "@mui/icons-material";
+import { Avatar, IconButton } from "@mui/material";
+import { FlexBox } from "lib/ui-ux";
+import { getInitialsByName } from "lib/utils";
+import ReactQuill from "react-quill";
+import styled from "styled-components";
+
+interface IEditorSectionProps {
+    from: string;
+    editorValue: string;
+    onCancelClick: () => void;
+    onSendClick: () => void;
+    onChange: (editorValue: string) => void
+}
+
+const EditorContainer = styled(FlexBox)`
+    .quill {
+        width: calc(100% - 50px);
+        display: flex;
+        flex-direction: column;
+    }
+    .quill:hover, :focus {
+            border-radius: 16px;
+            box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+    }
+    .ql-toolbar {
+        border-radius: 16px 16px 0px 0px;
+    }
+    .ql-container {
+        border-radius: 0px 0px 16px 16px;
+        min-height: 180px;
+    }
+`;
+
+export const EditorSection = (props: IEditorSectionProps) => {
+    const { from, editorValue, onCancelClick, onChange, onSendClick } = props;
+    return (
+        <>
+            <FlexBox $flexDirection="column" $height="auto" style={{ position: 'relative' }}>
+                <EditorContainer $gap="10px" $width="calc(100% - 10px)">
+                    <Avatar>{getInitialsByName(from)}</Avatar>
+                    <ReactQuill
+                        theme="snow"
+                        value={editorValue}
+                        placeholder="Type in here"
+                        preserveWhitespace
+                        onChange={onChange} />
+                </EditorContainer>
+                <FlexBox>
+                    <IconButton onClick={onSendClick} color="primary" sx={{ position: 'absolute', top: '1px', right: '10px' }}>
+                        <Send />
+                    </IconButton>
+                    <IconButton onClick={onCancelClick} sx={{ position: 'absolute', top: '1px', right: '50px' }}>
+                        <Delete />
+                    </IconButton>
+                </FlexBox>
+            </FlexBox>
+        </>
+    )
+}
--- src/modules/tickets/components/ticket-details/ticket-conversation/email-conversations/email-conversations.tsx
+++ src/modules/tickets/components/ticket-details/ticket-conversation/email-conversations/email-conversations.tsx
@@ -8,6 +8,7 @@ const emailConversations = [{
     `,
     from: 'Siddarth Menon',
     fromEmail: 'siddarth.menon@gmail.com',
+    toEmail: 'koppresh@gmail.com',
     createdDate: '2023-12-12T08:51:28.132Z',
     threadId: '100'
 }, {
@@ -19,14 +20,15 @@ const emailConversations = [{
     </div></div></div></div>`,
     from: 'Koppresh Putpak',
     fromEmail: 'koppresh@gmail.com',
+    toEmail: 'siddarth.menon@gmail.com',
     createdDate: '2023-10-17T15:45:30.715Z',
     threadId: '101'
 }]
 
-export const EmailConversations = () => {
+export const EmailConversations = (props: { subject: string }) => {
     const [emailThreads, setEmailThreads] = useState(emailConversations);
 
-    const onSend = useCallback((args: IEmailThreadProps, linkedThreadId: string) => {
+    const onSend = useCallback((args: Omit<IEmailThreadProps, 'subject'>, linkedThreadId: string) => {
         const clonedEmailThreads = emailThreads.slice()
         clonedEmailThreads.splice(emailThreads.findIndex((item) => item.threadId === linkedThreadId) + 1, 0, args)
         setEmailThreads(clonedEmailThreads);
@@ -34,7 +36,7 @@ export const EmailConversations = () => {
 
     return (
         <FlexBox $width="100%" $height="calc(100% - 32px)" $flexDirection="column" $gap="20px" $overflowY="auto">
-            {emailThreads.map((singleEmail, index) => <EmailCard key={index} emailProps={singleEmail} onSend={onSend} />)}
+            {emailThreads.map((singleEmail, index) => <EmailCard key={index} emailProps={{ ...singleEmail, subject: props.subject }} onSend={onSend} />)}
         </FlexBox>
     )
 }
