2b7f58733b962bcddd27cb7b6b7b2730b5d951f7
Siddarth Menon <siddarth.menon@agilepoint.com>
2024-01-07 14:19:10 +0530

    customer page display table data

---
+++ src/modules/customers/components/customer-views.tsx
@@ -0,0 +1,73 @@
+import { Typography } from "@mui/material";
+import { FlexBox } from "lib/ui-ux";
+import { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const CustomerViewsWrapper = styled(FlexBox)`
+    width: 200px;
+    height: 100%;
+    padding: 20px 0px;
+    border-style: solid;
+    border-color: #E5EAF2;
+    border-width: 0;
+    border-right-width: thin;
+`;
+
+const OptionWrapper = styled.div`
+  padding: 12px 14px;
+  cursor: pointer;
+  box-sizing: border-box;
+`;
+
+const customerViewOptions = [
+    {
+        name: 'Indivisual',
+        route: 'indivisual',
+        primaryKey: 'indivisual'
+    },
+    {
+        name: 'Organzation',
+        route: 'organzation',
+        primaryKey: 'organzation'
+    },
+    {
+        name: 'Channel Partner',
+        route: 'channel-partner',
+        primaryKey: 'channel-partner'
+    },
+]
+
+export const CustomerViews = () => {
+
+    return (
+        <CustomerViewsWrapper $flexDirection="column">
+            {customerViewOptions.map((data) => <CustomerViewOptions name={data.name} route={data.route} key={data.primaryKey} />)}
+        </CustomerViewsWrapper>
+
+    );
+};
+
+interface ICustomerViewOptionsProps {
+    name: string;
+    route: string;
+}
+
+const CustomerViewOptions = (props: ICustomerViewOptionsProps) => {
+    const { name, route } = props;
+    const navigate = useNavigate();
+    
+    const onLinkClick = useCallback(() => {
+        navigate(route);
+    }, [navigate, route]);
+
+    return (
+        <>
+            <OptionWrapper onClick={onLinkClick}>
+                <Typography variant="h6" color="inherit">
+                    {name}
+                </Typography>
+            </OptionWrapper>
+        </>
+    )
+}
---
+++ src/modules/customers/components/indivisual-customers.tsx
@@ -0,0 +1,56 @@
+import { createColumnHelper } from "@tanstack/react-table";
+import { DataGrid } from "lib/ui-ux";
+
+interface ICustomerData {
+    customerId: string;
+    name: string;
+    zone: string;
+    customerCode: string;
+    accountManager: string;
+}
+
+const useColumns = () => {
+    const columnHelper = createColumnHelper<ICustomerData>();
+
+    const columns = [
+        columnHelper.accessor('customerId', {
+            header: 'Account Manager',
+            id: 'customerId',
+            cell: props => props.getValue()
+        }),
+        columnHelper.accessor('name', {
+            header: 'Customer Name',
+            id: 'name',
+            cell: props => props.getValue()
+        }),
+        columnHelper.accessor('zone', {
+            header: 'Zone',
+            id: 'zone',
+            cell: props => props.getValue()
+        }),
+        columnHelper.accessor('customerCode', {
+            header: 'Customer Code',
+            id: 'customerCode',
+            cell: props => props.getValue()
+        }),
+        columnHelper.accessor('accountManager', {
+            header: 'Key Account Manager',
+            id: 'accountManager',
+            cell: props => props.getValue()
+        }),
+    ]
+
+    return columns;
+}
+
+interface IIndivisualCustomersProps {
+    data: ICustomerData[];
+}
+
+export const IndivisualCustomers = (props: IIndivisualCustomersProps) => {
+    const { data } = props;
+    const columns = useColumns()
+    return (
+        <DataGrid columns={columns} data={data} />
+    )
+}
