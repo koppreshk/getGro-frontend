f35ac4ceef2346c58517124867efe08c1f2817f2
koppresh <koppresh.putpak@agilepoint.com>
2024-01-07 11:49:55 +0530

    fix: completed email listing

--- src/modules/tickets/containers/ticket-conversation-container.tsx
+++ src/modules/tickets/containers/ticket-conversation-container.tsx
@@ -1,4 +1,4 @@
-import { CircularProgress } from "@mui/material";
+import { Alert, CircularProgress } from "@mui/material";
 import { useFetchTicketById, useGetTicketDetailsById, useTicketConversation } from "../apis";
 import { TicketConversationLayout } from "../components/ticket-details/ticket-conversation"
 import { FlexBox } from "lib/ui-ux";
@@ -6,12 +6,20 @@ import { FlexBox } from "lib/ui-ux";
 export const TicketConversationContainer = () => {
     const { isLoading, data } = useTicketConversation();
     const { data: ticketDetailsById, isLoading: ticketDetailsLoading } = useGetTicketDetailsById();
-    const { data: conversationsData, isLoading: conversationLoading } = useFetchTicketById();
+    const { data: conversationsData, isLoading: conversationLoading, isError } = useFetchTicketById();
 
     if (conversationLoading) {
         return (
             <FlexBox $alignItems="center" $justifyContent="center" $width="100%" $height="100%">
                 <CircularProgress />
+            </FlexBox>
+        )
+    }
+    //Temp code, should be removed!
+    if (conversationsData?.subject === undefined || isError) {
+        return (
+            <FlexBox $alignItems="center" $justifyContent="center" $width="100%" $height="100%">
+                <Alert sx={{ height: 'fit-content' }} severity="error">There was an error while fetching the data, please retry later</Alert>
             </FlexBox>
         )
     }
