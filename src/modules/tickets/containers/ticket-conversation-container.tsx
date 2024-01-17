922bba6dde046b1099fb40622c7edbf835124158
Siddarth Menon <siddarth.menon@agilepoint.com>
2024-01-17 18:49:22 +0530

    feat: Added skeleton loader to email conversation

--- src/modules/tickets/containers/ticket-conversation-container.tsx
+++ src/modules/tickets/containers/ticket-conversation-container.tsx
@@ -1,7 +1,8 @@
-import { Alert, CircularProgress } from "@mui/material";
+import { Alert } from "@mui/material";
 import { useFetchTicketById, useGetTicketDetailsById, useTicketConversation } from "../apis";
 import { TicketConversationLayout } from "../components/ticket-details/ticket-conversation"
 import { FlexBox } from "lib/ui-ux";
+import { EmailSkeletonLoader } from "lib/ui-ux/loader-components";
 
 export const TicketConversationContainer = () => {
     const { isLoading, data } = useTicketConversation();
@@ -10,8 +11,8 @@ export const TicketConversationContainer = () => {
 
     if (conversationLoading) {
         return (
-            <FlexBox $alignItems="center" $justifyContent="center" $width="100%" $height="100%">
-                <CircularProgress />
+            <FlexBox $width="100%">
+                <EmailSkeletonLoader/>
             </FlexBox>
         )
     }
