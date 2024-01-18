c8a3714ac60f02f84a92c9717e80a7e7f14614b0
koppresh <koppresh.putpak@agilepoint.com>
2024-01-18 18:03:14 +0530

    fix: Fixed print preview

--- src/modules/tickets/components/ticket-details/ticket-conversation/email-conversations/email-conversations-layout.tsx
+++ src/modules/tickets/components/ticket-details/ticket-conversation/email-conversations/email-conversations-layout.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Typography } from "@mui/material"
 import { CustomIconButton, FlexBox } from "lib/ui-ux"
 import styled from "styled-components"
@@ -6,6 +6,7 @@ import { EmailConversations } from "./email-conversations";
 import { UnfoldMore, UnfoldLess, Print } from '@mui/icons-material';
 import { Conversations, ITicketById } from "modules/tickets/apis";
 import { toCamelCasedKeysFromUnderScores } from "lib/utils";
+import './printable-content.css';
 
 const LayoutWrapper = styled(FlexBox)`
     padding: 15px 0px 15px 10px;
@@ -20,8 +21,6 @@ export const EmailConversationLayout = (props: { conversationsData: ITicketById 
     const { subject, conversations } = conversationsData;
     const casedConversation = conversations.map(item => ({ ...toCamelCasedKeysFromUnderScores(item), isCollapsed: true })) as IEmailConversations[];
     const [emailThreads, setEmailThreads] = useState(casedConversation);
-    const containerRef = useRef<HTMLDivElement>(null);
-    const iframeRef = useRef<HTMLIFrameElement>(null);
 
     useEffect(() => {
         if (casedConversation.length !== emailThreads.length) {
@@ -30,17 +29,7 @@ export const EmailConversationLayout = (props: { conversationsData: ITicketById 
     }, [casedConversation, casedConversation.length, emailThreads.length]);
 
     const onPrintHandler = () => {
-        if (containerRef.current && iframeRef.current) {
-            const content = containerRef.current;
-            const pri = iframeRef.current.contentWindow;
-            if (pri) {
-                pri.document.open();
-                pri.document.write(content.innerHTML);
-                pri.document.close();
-                pri.focus();
-                pri.print();
-            }
-        }
+        window.print();
     }
 
     const onSingleEmailCollapseHandler = (args: { messageId: string, isCollapsed: boolean }) => {
@@ -63,16 +52,15 @@ export const EmailConversationLayout = (props: { conversationsData: ITicketById 
 
     return (
         <>
-            <iframe ref={iframeRef} id="ifmcontentstoprint" style={{ display: 'none' }} />
-            <LayoutWrapper elementRef={containerRef} flexDirection="column" gap="10px" width="100%" height="100%">
+            <LayoutWrapper id="printable-content" flexDirection="column" gap="10px" width="100%" height="100%">
                 <FlexBox justifyContent="space-between" alignItems="center" padding="0px 16px 0px 0px">
                     <Typography variant="h5" title={subject} textOverflow={'ellipsis'} overflow={'hidden'} whiteSpace='nowrap' width={'calc(100% - 90px)'}>{subject}</Typography>
                     <FlexBox gap="10px">
                         {
                             isCollapsedAll ?
-                                <CustomIconButton tooltipProps={{ title: 'Expand all' }} iconComponent={<UnfoldMore />} sx={{ width: '24px', height: '24px' }} onClick={onExpandAll} />
+                                <CustomIconButton className="no-print" tooltipProps={{ title: 'Expand all' }} iconComponent={<UnfoldMore />} sx={{ width: '24px', height: '24px' }} onClick={onExpandAll} />
                                 :
-                                <CustomIconButton tooltipProps={{ title: 'Collapse all' }} iconComponent={<UnfoldLess />} sx={{ width: '24px', height: '24px' }} onClick={onCollapseAll} />
+                                <CustomIconButton className="no-print" tooltipProps={{ title: 'Collapse all' }} iconComponent={<UnfoldLess />} sx={{ width: '24px', height: '24px' }} onClick={onCollapseAll} />
                         }
                         <CustomIconButton tooltipProps={{ title: 'Print all' }} iconComponent={<Print />} sx={{ width: '24px', height: '24px' }} onClick={onPrintHandler} />
                     </FlexBox>
@@ -85,11 +73,4 @@ export const EmailConversationLayout = (props: { conversationsData: ITicketById 
             </LayoutWrapper>
         </>
     )
-}
-
-export const EmailConversationLayoutx = () => {
-
-    // return(
-
-    // )
 }
