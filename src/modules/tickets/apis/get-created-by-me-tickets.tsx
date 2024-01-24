732fc39d09dc3cc30faf0b3487e0f3e4abb4ca3b
Siddarth Menon <siddarth.menon@agilepoint.com>
2024-01-24 15:33:35 +0530

    fix: api issue and css overflow

--- src/modules/tickets/apis/get-created-by-me-tickets.tsx
+++ src/modules/tickets/apis/get-created-by-me-tickets.tsx
@@ -13,7 +13,7 @@ export const useGetCreatedByMeTickets = () => {
     const pageNumber = searchParams.get('pageNumber'); 
     const _pageNumber = pageNumber === undefined ? '' : `page=${pageNumber ?? '1'}&`;
 
-    const getCreatedByMeTickets = React.useCallback(() => getData(`${TicketsEndPoint.GET_CREATED_BY_ME_TICKETS}?${_pageNumber}&items_per_page=${itemsPerPage ?? '10'}`).then((res) => res.json()), [_pageNumber, getData, itemsPerPage]);
+    const getCreatedByMeTickets = React.useCallback(() => getData(`${TicketsEndPoint.GET_CREATED_BY_ME_TICKETS}?${_pageNumber}items_per_page=${itemsPerPage ?? '10'}`).then((res) => res.json()), [_pageNumber, getData, itemsPerPage]);
 
     return useQuery<{data: ITicketDetails[], total_pages: number}>({
         queryFn: getCreatedByMeTickets,
