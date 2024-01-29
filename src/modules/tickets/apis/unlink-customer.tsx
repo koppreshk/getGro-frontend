2cb84679102477a96477bedf99f69449db6c55cb
koppresh <koppresh.putpak@agilepoint.com>
2024-01-29 12:54:29 +0530

    fix: More changes

--- src/modules/tickets/apis/unlink-customer.tsx
+++ src/modules/tickets/apis/unlink-customer.tsx
@@ -2,10 +2,12 @@ import { useServiceClient } from "lib";
 import { useCallback } from "react";
 import { useMutation, useQueryClient } from "react-query";
 import { TicketsEndPoint, TicketsQueryKey } from "./api-enums";
+import { useGetQueryEndPoint } from "../containers";
 
 export const useUnlinkCustomer = () => {
     const { postData } = useServiceClient();
     const queryClient = useQueryClient();
+    const queryKey = useGetQueryEndPoint();
 
     const unLinkCustomer = useCallback((args: { ticketId: string }) =>
         postData(`${TicketsEndPoint.UNLINK_CUSTOMER}?ticket_id=${args.ticketId}`).then((res) => res.json()), [postData]);
@@ -14,7 +16,7 @@ export const useUnlinkCustomer = () => {
         mutationKey: [TicketsQueryKey.UNLINK_CUSTOMER],
         mutationFn: unLinkCustomer,
         onSuccess: () => {
-            queryClient.invalidateQueries(TicketsQueryKey.GET_ALL_TICKETS);
+            queryClient.invalidateQueries(queryKey);
         }
     });
 }
--- src/modules/tickets/containers/ticket-list-view-container.tsx
+++ src/modules/tickets/containers/ticket-list-view-container.tsx
@@ -5,24 +5,30 @@ import { TicketListViewLoader } from "lib/ui-ux/loader-components";
 import { TicketListView } from "../components/ticket-details/ticket-list-view"
 import { TicketsEndPoint } from "../apis/api-enums";
 
-const getQueryEndPointByType = (ticketType: string): keyof typeof TicketsEndPoint => {
-    switch (ticketType) {
-        case 'all':
-            return 'FETCH_ALL_TICKETS';
-        case 'unassigned':
-            return 'FETCH_ALL_UNASSIGNED';
-        case 'all-pending':
-            return 'FETCH_ALL_PENDING_TICKETS';
-        case 'all-complete':
-            return 'FETCH_ALL_COMPLETED_TICKETS';
-        case 'created-by-me':
-            return 'GET_CREATED_BY_ME_TICKETS';
-        default: return 'FETCH_ALL_TICKETS';
-    }
-}
-export const TicketListViewContainer = () => {
+export const useGetQueryEndPoint = () => {
     const match = useMatch('/tickets/:ticketType/:ticketId');
-    const queryEndPoint = getQueryEndPointByType(match?.params.ticketType || '');
+
+    const getQueryEndPointByType = (): keyof typeof TicketsEndPoint => {
+        switch (match?.params.ticketType || '') {
+            case 'all':
+                return 'FETCH_ALL_TICKETS';
+            case 'unassigned':
+                return 'FETCH_ALL_UNASSIGNED';
+            case 'all-pending':
+                return 'FETCH_ALL_PENDING_TICKETS';
+            case 'all-complete':
+                return 'FETCH_ALL_COMPLETED_TICKETS';
+            case 'created-by-me':
+                return 'GET_CREATED_BY_ME_TICKETS';
+            default: return 'FETCH_ALL_TICKETS';
+        }
+    }
+
+    return getQueryEndPointByType()
+}
+
+export const TicketListViewContainer = () => {
+    const queryEndPoint = useGetQueryEndPoint();
     const { data, isLoading } = useGetTicketsDataByKey(queryEndPoint, queryEndPoint);
 
     if (isLoading) {
