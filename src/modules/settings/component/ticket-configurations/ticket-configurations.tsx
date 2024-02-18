9bc29bbfdccf5ff2806c70b76009f43faf237a75
Siddarth Menon <siddarth.menon@agilepoint.com>
2024-02-18 13:15:10 +0530

    feat: Improve configuration UI

--- src/modules/settings/component/ticket-configurations/ticket-configurations.tsx
+++ src/modules/settings/component/ticket-configurations/ticket-configurations.tsx
@@ -1,52 +1,114 @@
 import { TaskOutlined } from "@mui/icons-material";
 import { Typography } from "@mui/material";
-import { FlexBox } from "lib/ui-ux";
+import { FlexBox, GridLayout } from "lib/ui-ux";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
+interface ICategoryOptions {
+    route: string;
+    label: string;
+    uniqueKey: string;
+}
 
-const ticketConfigOptions = [
+interface IConfigCategory {
+    categoryName: string;
+    categoryOptions: ICategoryOptions[];
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+interface IConfigurations {
+    configurations: IConfigCategory[];
+}
+
+const configurations = [
     {
-        route: "disposition-type",
-        label: "Disposition Type",
-        uniqueKey: "disposition-type"
+        categoryName: "Tickets",
+        categoryOptions: [
+            {
+                route: "disposition-type",
+                label: "Disposition Type",
+                uniqueKey: "disposition-type"
+            },
+            {
+                route: "ticket-filter",
+                label: "Ticket Filter",
+                uniqueKey: "ticket-filter"
+            },
+            {
+                route: "ticket-escalation",
+                label: "Ticket Escalation",
+                uniqueKey: "ticket-escalation"
+            },
+            {
+                route: "ticket-queue",
+                label: "Ticket Queue",
+                uniqueKey: "ticket-queue"
+            }
+        ]
     },
     {
-        route: "ticket-filter",
-        label: "Ticket Filter",
-        uniqueKey: "ticket-filter"
+        categoryName: "General",
+        categoryOptions: [
+            {
+                route: "access-configuration",
+                label: "Access Configuration",
+                uniqueKey: "general-access-configuration"
+            },
+            {
+                route: "ticket-filter",
+                label: "Chat Configuration",
+                uniqueKey: "general-ticket-filter"
+            },
+            {
+                route: "ticket-escalation",
+                label: "Product Configuration",
+                uniqueKey: "general-ticket-escalation2"
+            },
+            {
+                route: "ticket-queue",
+                label: "Agent Assistant Configuration",
+                uniqueKey: "general-ticket-queue1"
+            }, {
+                route: "access-configuration",
+                label: "Dashboard Configuration",
+                uniqueKey: "general-access-configuration1"
+            },
+            {
+                route: "ticket-filter",
+                label: "Gen AI Configuration",
+                uniqueKey: "general-ticket-filter1"
+            },
+            {
+                route: "ticket-escalation",
+                label: "Ticket Escalation",
+                uniqueKey: "general-ticket-escalation1"
+            },
+            {
+                route: "ticket-queue",
+                label: "Ticket Queue",
+                uniqueKey: "general-ticket-queue1"
+            }
+        ]
     },
-    {
-        route: "ticket-escalation",
-        label: "Ticket Escalation",
-        uniqueKey: "ticket-escalation"
-    },
-    {
-        route: "ticket-queue",
-        label: "Ticket Queue",
-        uniqueKey: "ticket-queue"
-    }
+
 ]
 
 const ConfigLinkWrapper = styled.div`
     :hover {
         background: ${(props) => props.theme.pallete.purpleLight};
     }
+    background-color: white;
 `;
 
-const OptionWrapper = styled.div`
-  padding: 8px 5px;
+const OptionWrapper = styled(FlexBox)`
+  padding: 0 15px;
   cursor: pointer;
-  box-sizing: border-box;
+  border-radius: 5px;
+  border: 1px solid #e3e3e3;
+  height: 50px;
 `;
 
-interface ITicketConfigOptionsProps {
-    route: string;
-    label: string;
-    uniqueKey: string;
-}
-
-const TicketConfigOptions = (props: ITicketConfigOptionsProps) => {
+const TicketConfigOptions = (props: ICategoryOptions) => {
     const { label, route } = props;
     const navigate = useNavigate();
     const OnLinkClick = () => {
@@ -55,7 +117,7 @@ const TicketConfigOptions = (props: ITicketConfigOptionsProps) => {
 
     return (
         <ConfigLinkWrapper onClick={OnLinkClick}>
-            <OptionWrapper>
+            <OptionWrapper alignItems="center" justifyContent="center">
                 <Typography variant="body3"> {label} </Typography>
             </OptionWrapper>
         </ConfigLinkWrapper>
@@ -64,17 +126,24 @@ const TicketConfigOptions = (props: ITicketConfigOptionsProps) => {
 
 export const TicketsConfiguration = () => {
     return (
-        <FlexBox padding="20px" flexDirection="column" gap="10px">
-            <FlexBox alignItems="center" gap="5px">
-                <TaskOutlined color="primary" />
-                <Typography variant="h5">
-                    Tickets
-                </Typography>
-            </FlexBox>
+        <>
+            {
+                configurations.map((data) =>
+                (
+                    <FlexBox padding="20px" flexDirection="column" gap="10px" width="100%">
+                        <FlexBox alignItems="center" gap="5px" padding="0 10px">
+                            <TaskOutlined color="primary" />
+                            <Typography variant="h5">
+                                {data.categoryName}
+                            </Typography>
+                        </FlexBox>
+                        <GridLayout $padding="10px" $gridGap="10px" $gridTemplateColumns={"repeat(6, 1fr)"}>
+                            {data.categoryOptions.map((categoryData) => <TicketConfigOptions key={categoryData.uniqueKey} uniqueKey={categoryData.uniqueKey} label={categoryData.label} route={categoryData.route} />)}
+                        </GridLayout>
+                    </FlexBox>
+                ))
+            }
+        </>
 
-            <FlexBox flexDirection="column">
-                {ticketConfigOptions.map((data) => <TicketConfigOptions key={data.uniqueKey} uniqueKey={data.uniqueKey} label={data.label} route={data.route} />)}
-            </FlexBox>
-        </FlexBox>
     )
 }
