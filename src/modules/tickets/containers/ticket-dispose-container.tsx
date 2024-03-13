822e2964a39756f8ff12f3b90ee272960ba61474
Siddharth1097 <siddarth@hiranya-garbha.com>
2024-03-13 20:30:29 +0530

    fix: payload error

--- src/modules/tickets/containers/ticket-dispose-container.tsx
+++ src/modules/tickets/containers/ticket-dispose-container.tsx
@@ -18,14 +18,17 @@ export const TicketDisposeContainer = (props: ITicketDisposeContainerProps) => {
     const onToggleTicketDispose = () => setTicketDisposeDrawer((prevalue) => !prevalue);
 
     const submitDisposeTicket = React.useCallback((data: IDispostionFormFields) => {
-        mutateAsync({
+        const payload = {
             dispositionId: data.dispositionId,
             callBackTime: data.callBackTime,
-            employeeId: data.employeeId ? Number(data.employeeId[0].key): undefined,
             queueId: data.queueId,
             remarks: data.remarks,
-            tagId:  data.tagId ? Number(data.tagId[0].key) : undefined
-        })
+            tagId: data.tagId ? Number(data.tagId[0].key) : undefined
+        };
+
+        data.employeeId && (payload["employeeId"] = Number(data.employeeId[0].key));
+
+        mutateAsync(payload)
             .then(() => {
                 navigate('/tickets', { replace: true });
                 showNotification({ message: 'Successfully disposed ticket', type: 'success' });
