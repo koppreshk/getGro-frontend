7efa04fa06ffd46d5684dbe796ed3b5bbf9c0b84
Siddarth Menon <siddarth.menon@agilepoint.com>
2024-04-29 18:05:59 +0530

    feat: Added new dashobaord panel design

--- src/modules/dashboard/components/dashboard-layout.tsx
+++ src/modules/dashboard/components/dashboard-layout.tsx
@@ -1,18 +1,18 @@
 import { FlexBox } from "lib/ui-ux"
-import { DashboardCategoriesPanel } from "./parts/categories-panel"
+import { DashboardCategoriesPanel2 } from "./parts/categories-panel"
 import { Typography } from "@mui/material";
 import { useAuth } from "modules/login";
 import styled, { useTheme } from "styled-components";
 
 const StyledFlexbox = styled(FlexBox)`
-    background-color: ${(props) => props.theme.pallete.white};
+    /* background-color: ${(props) => props.theme.pallete.white}; */
 `;
 
 export const DashboardLayout = () => {
     return (
         <FlexBox flexDirection="column" height="100%" width="100%">
             <UserDetails />
-            <DashboardCategoriesPanel />
+            <DashboardCategoriesPanel2 />
         </FlexBox>
     )
 };
@@ -21,7 +21,7 @@ const UserDetails = () => {
     const { user } = useAuth();
     const { pallete } = useTheme();
     return (
-        <StyledFlexbox flexDirection="column" gap="5px" padding="25px 25px 0">
+        <StyledFlexbox flexDirection="column" gap="5px" padding="25px 25px 20px">
             <Typography variant="h2" textTransform={"capitalize"}>Hi {user?.email.split('@')[0]}</Typography>
             <Typography variant="h6" sx={{ color: pallete.grayVariant2 }}>Welcome back!</Typography>
         </StyledFlexbox>
--- src/modules/dashboard/components/parts/categories-panel.tsx
+++ src/modules/dashboard/components/parts/categories-panel.tsx
@@ -1,6 +1,9 @@
-import React from "react";
-import { Box, Tab, Tabs } from "@mui/material"
+import React, { useEffect } from "react";
+import { Box, Tab, Tabs, Typography } from "@mui/material"
 import { AgentPerformanceDashContainer, TicketMonitoringDashContainer } from "modules/dashboard/container";
+import styled, { useTheme } from "styled-components";
+import { FlexBox } from "lib/ui-ux";
+import { Widgets } from "@mui/icons-material";
 
 interface IDashboardCategories {
     id: number;
@@ -8,10 +11,26 @@ interface IDashboardCategories {
     component: JSX.Element;
 }
 
+const StyledTabs = styled(Tabs)`
+   /* &&{
+     .MuiTabs-indicator {
+        display: flex;
+        justify-content: center;
+        background-color: transparent;
+      }
+
+      .MuiTabs-indicatorSpan {
+        max-width: 40;
+        width: 100%;
+        background-color: #635ee7;
+    }
+    } */
+`;
+
 const DashboardCategories: IDashboardCategories[] = [
     {
         id: 1,
-        name: "Ticket Monitoring Dashboard",
+        name: "Tickets Monitor",
         component: <TicketMonitoringDashContainer />,
     },
     {
@@ -26,7 +45,7 @@ const DashboardCategories: IDashboardCategories[] = [
     },
     {
         id: 4,
-        name: "Customer Satisfaction Report",
+        name: "CSR Dashboard",
         component: <AgentPerformanceDashContainer />,
     },
     {
@@ -46,11 +65,11 @@ export const DashboardCategoriesPanel = () => {
     return (
         <>
             <Box sx={{ borderBottom: 1, borderColor: '#cccc', bgcolor: '#ffff', padding: '0px 25px' }}>
-                <Tabs value={value} onChange={handleChange} aria-label="dashboard categories tabs">
+                <StyledTabs value={value} onChange={handleChange} aria-label="dashboard categories tabs" >
                     {DashboardCategories.map((category) => {
                         return <Tab key={category.id} label={category.name} id={`dashboard-tab-${category.id}`} value={category.id} sx={{ textTransform: 'none' }} />
                     })}
-                </Tabs>
+                </StyledTabs>
             </Box>
             <Box sx={{ width: '100%', height: '100%', overflowY: 'auto' }}>
                 <CustomTabPanel index={value} value={value} />
@@ -83,3 +102,66 @@ function CustomTabPanel(props: TabPanelProps) {
         </div>
     );
 }
+
+
+const TabPillWrapper = styled(FlexBox) <{ $isSelected: boolean }>`
+    border-radius: 20px;
+    cursor: pointer;
+    background-color: ${({ $isSelected, theme}) => $isSelected ? theme.pallete.primaryPurple : '#e5e5ea'};
+    color: ${({ $isSelected, theme}) => $isSelected ? theme.pallete.white : 'black'};
+`;
+
+interface ITabPillProps {
+    label: string;
+    id: number;
+    onClickHandler: (id: number) => void;
+    value: number
+}
+
+const TabPill = (props: ITabPillProps) => {
+    const { label, id, onClickHandler, value } = props;
+    const [isSelected, setIsSelected] = React.useState(false)
+    const { pallete } = useTheme();
+
+    useEffect(() => {
+        setIsSelected(() => value === id);
+    }, [id, value]);
+
+    return (
+        <TabPillWrapper onClick={() => onClickHandler(id)} padding="10px 15px" justifyContent="space-between" alignItems="center" id={`dashboard-tab-${id}`} $isSelected={isSelected}>
+            <Typography variant="caption" sx={{ color: `${isSelected ? pallete.white : pallete.grayVariant2}`  }}>{label}</Typography>
+        </TabPillWrapper>
+    )
+}
+
+export const DashboardCategoriesPanel2 = () => {
+    const [value, setValue] = React.useState(1);
+    
+
+    const onClickHandler = (id: number) => {
+        setValue(id);
+    };
+
+    const { pallete } = useTheme();
+
+    return (
+        <>
+            <Box sx={{ padding: '0px 25px' }}>
+                <FlexBox aria-label="dashboard categories tabs" gap='10px' flexDirection="row" alignItems="center">
+                    <FlexBox alignItems="center" gap="5px" padding="0 12px 0 0">
+                        <Widgets color="primary" />
+                        <Typography variant="h4" sx={{ color: pallete.grayVariant2 }} >Dashboards</Typography>
+                    </FlexBox>
+
+                    {DashboardCategories.map((category) => {
+                        return <TabPill key={category.id} label={category.name} id={category.id} onClickHandler={onClickHandler} value={value}/>
+                    })}
+                </FlexBox>
+            </Box>
+            <Box sx={{ width: '100%', height: '100%', overflowY: 'auto' }}>
+                <CustomTabPanel index={value} value={value} />
+            </Box >
+        </>
+    )
+}
+
