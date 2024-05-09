f19d38c7879fd2e41439ab06ca36e23659fab2a7
Koppresh <koppresh@gmail.com>
2024-05-09 12:17:18 +0530

    fix: Added charts

--- src/modules/dashboard/components/parts/support-monitoring/ticket-stats.tsx
+++ src/modules/dashboard/components/parts/support-monitoring/ticket-stats.tsx
@@ -3,6 +3,10 @@ import { FlexBox } from "lib/ui-ux";
 import styled, { useTheme } from "styled-components";
 import { DateFilters } from "../tickets-monitor/date-filters";
 import { useState, useCallback } from "react";
+import ReactApexChart from "react-apexcharts";
+import { ApexOptions } from "apexcharts";
+import { FormProvider, useForm } from "react-hook-form";
+import { SelectField } from "lib/form-fields";
 
 const StyledContainer = styled(FlexBox)`
     background-color: ${({ theme }) => theme.pallete.white};
@@ -14,15 +18,12 @@ export const TicketStats = () => {
     const quickStats1 = [{
         name: 'Tickets Created',
         value: 1,
-        renderSeparator: true
     }, {
         name: 'Replies By Agent',
-        value: 5,
-        renderSeparator: true
+        value: 5
     }, {
         name: 'Response Pending',
-        value: 7,
-        renderSeparator: true
+        value: 7
     }];
 
     const quickStats2 = [{
@@ -49,13 +50,16 @@ export const TicketStats = () => {
                     <Typography variant="h5">Ticket Statistics</Typography>
                     <DateFilters onFilterChangeHandler={onFilterChangeHandler} filterValue={filterValue} dateFilterTypes={['Today', 'Yesterday', 'Last 7 Days', 'Last 30 Days', 'Last 90 Days']} />
                 </FlexBox>
-                <FlexBox gap="20px">
-                    <FlexBox flexDirection="column" gap="20px">
-                        {quickStats1.map((item) => <QuickStats key={item.name} item={item} />)}
+                <FlexBox width="100%">
+                    <FlexBox gap="20px" width="40%" padding="0px 0px 0px 50px">
+                        <FlexBox flexDirection="column" gap="60px" width="50%">
+                            {quickStats1.map((item) => <QuickStats key={item.name} item={item} />)}
+                        </FlexBox>
+                        <FlexBox flexDirection="column" gap="60px" width="50%">
+                            {quickStats2.map((item) => <QuickStats key={item.name} item={item} />)}
+                        </FlexBox>
                     </FlexBox>
-                    <FlexBox flexDirection="column" gap="20px">
-                        {quickStats2.map((item) => <QuickStats key={item.name} item={item} />)}
-                    </FlexBox>
+                    <TicketsCreated />
                 </FlexBox>
             </StyledContainer>
         </>
@@ -69,12 +73,57 @@ const QuickStats = (props: {
         renderSeparator?: boolean
     }
 }) => {
-    const { name, value, renderSeparator } = props.item;
-    const { pallete } = useTheme();
+    const { name, value } = props.item;
+    const { dashboard, pallete } = useTheme();
     return (
-        <FlexBox flexDirection="column" width="200px" style={{ borderRight: renderSeparator ? `1px solid ${pallete.grayNeutral}` : 'unset' }}>
-            <Typography sx={{ color: pallete.grayNeutral }} variant="subheading1">{name}</Typography>
-            <Typography variant="h5">{value}</Typography>
+        <FlexBox flexDirection="column" gap="10px" style={{ borderLeft: `3px solid ${dashboard.graphBgColor1}`, paddingLeft: '15px' }}>
+            <Typography sx={{ color: pallete.grayVariant3 }} variant="h6">{name}</Typography>
+            <Typography variant="h3">{value}</Typography>
         </FlexBox>
+    )
+}
+
+const TicketsCreated = () => {
+    const form = useForm({
+        defaultValues: {
+            groupBy: 'status'
+        }
+    });
+    const data = {
+        series: [{
+            data: [2, 10, 7]
+        }],
+        options: {
+            chart: {
+                type: 'bar',
+                height: 350,
+                fontFamily: 'Poppins',
+                id: 'apexchart-example'
+            },
+            plotOptions: {
+                bar: {
+                    borderRadius: 4,
+                    borderRadiusApplication: 'end',
+                    horizontal: true,
+                }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            xaxis: {
+                categories: ['Unassgined', 'Pending', 'Completed'],
+            }
+        } as ApexOptions
+    };
+    return (
+        <FormProvider {...form}>
+            <FlexBox flexDirection="column" width="60%">
+                <FlexBox justifyContent="space-between">
+                    <Typography variant="h6">Tickets Created</Typography>
+                    <SelectField menuOptions={[{ key: 'status', value: 'Status' }]} name="groupBy" label="Group By" size="small" sx={{ width: '200px' }} />
+                </FlexBox>
+                <ReactApexChart options={data.options} series={data.series} type="bar" height={285} width={'100%'} />
+            </FlexBox>
+        </FormProvider>
     )
 }
