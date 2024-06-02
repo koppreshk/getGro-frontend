8a6f135f97ed3f35f782a05a7ba733d7ed6424dd
Siddharth1097 <siddarth@hiranya-garbha.com>
2024-06-02 12:36:59 +0530

    feat: Added dialogs

--- src/modules/settings/component/apps/marketplace/shopify/shopify-header.tsx
+++ src/modules/settings/component/apps/marketplace/shopify/shopify-header.tsx
@@ -1,12 +1,20 @@
 /// <reference types="vite-plugin-svgr/client" />
 
+import React from "react";
 import { Button, Card, CardContent, Typography } from "@mui/material"
 import { FlexBox } from "lib/ui-ux";
 import ShopifyIcon from '../../../../../../assets/svg/shopify-icon.svg?react';
 import { useTheme } from "styled-components";
+import { AddStoreDialog } from "./add-store-dialog";
 
 export const ShopifyHeader = () => {
     const { pallete } = useTheme();
+    const [open, setOpen] = React.useState(false);
+
+    const toggleDialog = () => {
+        setOpen((prevValue) => !prevValue);
+    };
+
     return (
         <>
             <Card variant="elevation" elevation={1}>
@@ -19,7 +27,8 @@ export const ShopifyHeader = () => {
                                 <Typography variant="subheading1" sx={{ color: pallete.grayNeutral }}>Connect your Shopify store and display customer order information.</Typography>
                             </FlexBox>
                         </FlexBox>
-                        <Button variant="contained" size="small">Install</Button>
+                        <Button variant="contained" size="small" onClick={toggleDialog}>Install</Button>
+                        <AddStoreDialog onClose={toggleDialog} open={open} />
                     </FlexBox>
                 </CardContent>
             </Card>
