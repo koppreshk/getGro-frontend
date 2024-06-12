2f2f867080c05cc011a574895fc276f38aab16ae
Siddharth1097 <siddarth@hiranya-garbha.com>
2024-06-12 11:47:39 +0530

    feat: Added total logi hours

--- src/modules/dashboard/components/parts/agent-performnace/customer-satifaction.tsx
+++ src/modules/dashboard/components/parts/agent-performnace/customer-satifaction.tsx
@@ -3,6 +3,8 @@ import { FlexBox, GridLayout } from "lib/ui-ux"
 import { CustomCircularProgress } from "./first-contact-resolution"
 import styled, { useTheme } from "styled-components";
 import { SentimentSatisfiedOutlined, SentimentDissatisfiedOutlined, SentimentNeutralOutlined } from '@mui/icons-material';
+import ReactApexChart from "react-apexcharts";
+import { ApexOptions } from "apexcharts";
 
 const StyledContainer = styled(FlexBox)`
     background: ${({ theme }) => theme.pallete.white};
@@ -74,12 +76,47 @@ const SurveyResponse = (props: { value: string, subHeading: string }) => {
 }
 
 export const TotalLoginHours = () => {
+    const data = {
+        series: [5, 1, 0.5, 0.3],
+        options: {
+            chart: {
+                fontFamily: 'Poppins',
+                type: 'donut',
+            },
+            labels: ['Active', 'Busy', 'Away', 'Do not disturb', 'Offline'],
+            plotOptions: {
+                pie: {
+                    customScale: 0.8,
+                    donut: {
+                        labels: {
+                            show: true,
+                            total: {
+                                show: true,
+                                formatter(w) {
+                                    const total = w.globals.series.reduce((acc: number, curr: number) => acc += curr);
+                                    return `${total} hr`
+                                },
+                            }
+                        }
+                    }
+                }
+
+            },
+            tooltip: {
+                y: {
+                    formatter: function (val) {
+                        return val < 1 ? `${val * 60} min` : `${val} hr`
+                    }
+                }
+            }
+            // colors: ['#17e254', '#ec3427', '#ffef0e', '#d80e00', '#c9c2c2']
+        } as ApexOptions
+    };
+
     return (
         <StyledContainer gap="20px" flexDirection="column">
             <Typography variant="h6">Total Login Hours</Typography>
-            <FlexBox alignItems="center" height="100%" width="100%" justifyContent="center">
-                <Typography>No Results found</Typography>
-            </FlexBox>
+            <ReactApexChart options={data.options} series={data.series} type="donut" />
         </StyledContainer>
     )
 }
