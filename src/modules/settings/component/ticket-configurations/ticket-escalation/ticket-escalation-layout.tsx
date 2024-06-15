7cae28ece32adf6366f538eb7a992779f705b873
Siddharth1097 <siddarth@hiranya-garbha.com>
2024-06-15 20:20:43 +0530

    fix: Added layout

--- src/modules/settings/component/ticket-configurations/ticket-escalation/ticket-escalation-layout.tsx
+++ src/modules/settings/component/ticket-configurations/ticket-escalation/ticket-escalation-layout.tsx
@@ -1,15 +1,16 @@
 
 import React, { useCallback } from "react";
-import { useNavigate } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { Add } from "@mui/icons-material"
 import { Button, Typography } from "@mui/material"
-import { BreadCrumbs, CustomIconButton, DrawerExtended, FlexBox } from "lib/ui-ux"
-import { CreateTicketEscalationContainer } from "modules/settings/containers";
+import { BreadCrumbs, CustomIconButton, FlexBox } from "lib/ui-ux"
+// import { CreateTicketEscalationContainer } from "modules/settings/containers";
 import { EscalationConditions } from "modules/settings/apis/escalations";
 import TicketEscalationList from "./ticket-escalation-list";
 import { useAppDispatch } from "lib/hooks";
 import { setTotalPage } from "modules/settings/storage";
+import { AddEscalationLayout } from "./ticket-escalation-new/add-escalation-layout";
 
 export interface ITicketEscalaltionLayoutProps {
     isLoading: boolean;
@@ -17,32 +18,25 @@ export interface ITicketEscalaltionLayoutProps {
     totalPages: number;
 }
 
-const AddNewEscalation = (props: {
-    openAddEscalationDrawer: boolean;
-    toggleAddEscalationDrawer: () => void
-}) => {
-    const { openAddEscalationDrawer, toggleAddEscalationDrawer } = props;
-    return (
-        <DrawerExtended
-            width="800px"
-            header="Add New Escalation"
-            anchor="right"
-            open={openAddEscalationDrawer}
-            onRenderContent={() => (
-                <CreateTicketEscalationContainer toggleAddEscalationDrawer={toggleAddEscalationDrawer} />
-            )}
-            onClose={toggleAddEscalationDrawer} />
-    )
-}
+// const AddNewEscalation = (props: {
+//     openAddEscalationDrawer: boolean;
+//     toggleAddEscalationDrawer: () => void
+// }) => {
+//     const { openAddEscalationDrawer, toggleAddEscalationDrawer } = props;
+//     return (
+//         <DrawerExtended
+//             width="800px"
+//             header="Add New Escalation"
+//             anchor="right"
+//             open={openAddEscalationDrawer}
+//             onRenderContent={() => (
+//                 <CreateTicketEscalationContainer toggleAddEscalationDrawer={toggleAddEscalationDrawer} />
+//             )}
+//             onClose={toggleAddEscalationDrawer} />
+//     )
+// }
 
 export const TicketEscalationLayout = (props: ITicketEscalaltionLayoutProps) => {
-    const [openAddEscalationDrawer, setOpenAddEscalationDrawer] = React.useState(false);
-    const navigate = useNavigate();
-
-    const toggleAddEscalationDrawer = useCallback(() => {
-        setOpenAddEscalationDrawer((prevValue) => !prevValue)
-    }, []);
-
     const dispatch = useAppDispatch();
 
     React.useEffect(() => {
@@ -52,15 +46,32 @@ export const TicketEscalationLayout = (props: ITicketEscalaltionLayoutProps) => 
     return (
         <FlexBox width="100%" flexDirection="column">
             <BreadCrumbs />
+            <Routes>
+                <Route key='base-route' path="/" element={<EscalationLayoutContent {...props} />} />
+                <Route key='add-route' path="add-escalation" element={<AddEscalationLayout />} />
+            </Routes>
+        </FlexBox>
+    )
+}
+
+const EscalationLayoutContent = (props: ITicketEscalaltionLayoutProps) => {
+    const navigate = useNavigate();
+
+    const toggleAddEscalationDrawer = useCallback(() => {
+        navigate('add-escalation');
+    }, [navigate]);
+
+    return (
+        <>
             <FlexBox width="100%" justifyContent="space-between" padding="10px" alignItems="center">
                 <FlexBox alignItems="center" gap="10px">
                     <CustomIconButton onClick={() => { navigate('/configurations') }} iconComponent={<ArrowBackIcon />} tooltipProps={{ title: 'Back' }} />
                     <Typography variant="h5">Ticket Escalation</Typography>
                 </FlexBox>
                 <Button variant="contained" onClick={toggleAddEscalationDrawer} startIcon={<Add />}>Add Escalation</Button>
-                <AddNewEscalation openAddEscalationDrawer={openAddEscalationDrawer} toggleAddEscalationDrawer={toggleAddEscalationDrawer} />
+                {/* <AddNewEscalation openAddEscalationDrawer={openAddEscalationDrawer} toggleAddEscalationDrawer={toggleAddEscalationDrawer} /> */}
             </FlexBox>
             <TicketEscalationList {...props} />
-        </FlexBox>
-    )
+        </>
+    );
 }
---
+++ src/modules/settings/component/ticket-configurations/ticket-escalation/ticket-escalation-new/add-escalation-layout.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { Stepper, Box, Step, StepLabel, Typography, Button, DialogActions } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useForm, FormProvider } from "react-hook-form";
+
+const steps = [
+    {
+        label: 'Choose Condition',
+    },
+    {
+        label: 'SLA Targets',
+    },
+    {
+        label: 'Add Reminders',
+    },
+    {
+        label: 'Add Escalation',
+    }
+];
+
+export const AddEscalationLayout = () => {
+    const [activeStep, setActiveStep] = React.useState(0);
+    const navigate = useNavigate();
+    const form = useForm();
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const isLastStep = activeStep === steps.length - 1;
+
+    const onClose = () => navigate(-1);
+
+    return (
+        <Box sx={{ p: '0px 20px', mb: '10px' }}>
+            <FormProvider {...form}>
+                <AddEscalaltionSteps activeStep={activeStep} />
+                {activeStep === 0 ? <ChooseCondition /> : <span>Work in Progress..</span>}
+                <DialogActions>
+                    <Button variant="outlined" onClick={isLastStep ? handleBack : onClose}>
+                        {isLastStep ? 'Back' : 'Cancel'}
+                    </Button>
+                    <Button variant="contained" autoFocus onClick={isLastStep ? onClose : handleNext}>
+                        {isLastStep ? 'Save' : 'Next'}
+                    </Button>
+                </DialogActions>
+            </FormProvider>
+        </Box>
+    )
+}
+
+const AddEscalaltionSteps = (props: { activeStep: number }) => {
+    const { activeStep } = props;
+    return (
+        <>
+            <Box>
+                <Stepper activeStep={activeStep} orientation="horizontal">
+                    {steps.map((step, index) => (
+                        <Step key={step.label}>
+                            <StepLabel
+                                optional={
+                                    index === 3 ? (
+                                        <Typography variant="body3">Last step</Typography>
+                                    ) : null
+                                }
+                            >
+                                {step.label}
+                            </StepLabel>
+                        </Step>
+                    ))}
+                </Stepper>
+            </Box>
+        </>
+    )
+}
+
+const ChooseCondition = () => {
+    return (
+        <span>Test</span>
+    )
+}
