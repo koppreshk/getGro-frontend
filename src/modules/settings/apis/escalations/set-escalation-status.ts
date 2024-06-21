d7037bc515b89975fda286a6a2b12a19a1ec97bc
Siddharth1097 <siddarth@hiranya-garbha.com>
2024-06-21 15:29:12 +0530

    fix: Added more changes

--- src/modules/settings/apis/escalations/set-escalation-status.ts
+++ src/modules/settings/apis/escalations/set-escalation-status.ts
@@ -1,11 +1,10 @@
 import React from "react";
-import { useMutation, useQueryClient } from "react-query";
+import { useMutation } from "react-query";
 import { useServiceClient } from "lib";
 import { EscalationQueryKey, EscalationEndPoint } from "./api-enums";
 
 export const useSetEscalationStatus = () => {
     const { postData } = useServiceClient();
-    const queryClient = useQueryClient();
 
     const setEscalationStatus = React.useCallback((args: { id: number }) =>
         postData(`${EscalationEndPoint.SET_ESCALATION_STATUS}?id=${args.id}`).then((res) => res.json()), [postData]);
@@ -13,8 +12,5 @@ export const useSetEscalationStatus = () => {
     return useMutation({
         mutationKey: EscalationQueryKey.SET_ESCALATION_STATUS,
         mutationFn: setEscalationStatus,
-        onSuccess: () => {
-            queryClient.invalidateQueries(EscalationQueryKey.FETCH_ALL_ESCALATIONS_NEW);
-        }
     });
 }
