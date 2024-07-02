89cfbdf56e6364092d05d27dfd1f35d96873d31d
Siddharth1097 <siddarth@hiranya-garbha.com>
2024-07-02 22:03:05 +0530

    fix: Added api integ for cp

--- src/modules/tickets/containers/overview/manage-priority-container.tsx
+++ src/modules/tickets/containers/overview/manage-priority-container.tsx
@@ -1,16 +1,19 @@
 import { CenteredCircularProgress } from "lib/ui-ux";
-import { ITicketDetails, useFetchPriorities } from "modules/tickets/apis";
+import { ITicketDetails, useChangePriority, useFetchPriorities } from "modules/tickets/apis";
 import { ManagePriority } from "modules/tickets/components/ticket-details/ticket-details-section/ticket-overview"
 
-interface IManagePriorityContainerProps extends Pick<ITicketDetails, 'priority'> {
+interface IManagePriorityContainerProps extends Pick<ITicketDetails, 'priority' | 'ticketId'> {
 
 }
 
 export const ManagePriorityContainer = (props: IManagePriorityContainerProps) => {
     const { data, isLoading } = useFetchPriorities();
+    const { mutateAsync } = useChangePriority(props.ticketId);
 
     const onChangePriority = (newPriority: number) => {
-        console.log(newPriority);
+        return mutateAsync({
+            priorityId: newPriority
+        })
     }
 
     if (isLoading) {
