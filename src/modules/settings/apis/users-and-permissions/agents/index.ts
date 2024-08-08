a0eff049d91e14183235d682f314c314e84faa02
Siddharth1097 <siddarth@hiranya-garbha.com>
2024-08-08 22:43:27 +0530

    fix: added all apis

--- src/modules/settings/apis/users-and-permissions/agents/index.ts
+++ src/modules/settings/apis/users-and-permissions/agents/index.ts
@@ -4,4 +4,4 @@ export * from './create-user';
 export * from './fetch-all-roles';
 export * from './edit-user';
 export * from './activate-user';
-export * from './fetch-availability-statuses';
+export * from './fetch-agent-avail-status';
