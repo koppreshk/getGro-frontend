f015cda703b3b276b0d7d6c5dab6e825b7e50b77
Siddharth1097 <siddarth@hiranya-garbha.com>
2024-08-09 21:31:18 +0530

    fix: changed css on hover for card view

--- src/modules/tickets/components/tickets-card-view/card-view.tsx
+++ src/modules/tickets/components/tickets-card-view/card-view.tsx
@@ -19,7 +19,7 @@ const StyledCard = styled(FlexBox)`
     padding: 15px 20px 15px;
 
     &:hover {
-        background: #F1F2F4;
+        box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
     }
 `;
 
