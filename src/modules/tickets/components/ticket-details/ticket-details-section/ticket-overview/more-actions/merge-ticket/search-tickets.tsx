7fb843e558687fe573427c676c8b7bb200122834
Siddharth1097 <siddarth@hiranya-garbha.com>
2024-08-15 22:06:11 +0530

    fix: added onchange

--- src/modules/tickets/components/ticket-details/ticket-details-section/ticket-overview/more-actions/merge-ticket/search-tickets.tsx
+++ src/modules/tickets/components/ticket-details/ticket-details-section/ticket-overview/more-actions/merge-ticket/search-tickets.tsx
@@ -2,12 +2,19 @@ import { TextField, Typography } from "@mui/material"
 import { FlexBox } from "lib/ui-ux"
 
 export const SearchTickets = () => {
+
+    const onChange: React.ChangeEventHandler<HTMLInputElement> = (ev) => {
+        console.log(ev.target.value);
+    }
+
     return (
         <FlexBox flexDirection="column" gap={'5px'}>
             <TextField
                 name="searchText"
                 fullWidth placeholder="Search a secondary ticket by ID or Subject"
-                size="small" />
+                size="small"
+                autoFocus
+                onChange={onChange} />
             <Typography variant="body3">Search and add secondary tickets that you want to merge with primary tickets</Typography>
         </FlexBox>
     )
