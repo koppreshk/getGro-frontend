32292391d6ca065b963507d23783adacc5caa03b
Siddharth1097 <siddarth@hiranya-garbha.com>
2024-08-16 15:12:04 +0530

    fix: added api endpoints

---
+++ src/modules/tickets/apis/search-ticket.ts
@@ -0,0 +1,15 @@
+import useLazyQuery from "lib/hooks/react-query-utils";
+import { TicketsEndPoint, TicketsQueryKey } from "./api-enums";
+
+export interface ISearchTickets {
+    data: [],
+    total_pages: number,
+    current_page: number
+}
+
+export const useSearchTickets = () => {
+    return useLazyQuery<ISearchTickets[]>({
+        apiEndPoint: TicketsEndPoint.GET_CUSTOMER_DETAILS,
+        queryKey: TicketsQueryKey.GET_CUSTOMER_DETAILS
+    })
+}
