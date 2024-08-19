47fe6b035b3c678b652bab81ae04e348c1d20112
Siddharth1097 <siddarth@hiranya-garbha.com>
2024-08-19 13:47:37 +0530

    fix: added ticket history base ui

--- src/modules/tickets/components/ticket-details/history/email-history.tsx
+++ src/modules/tickets/components/ticket-details/history/email-history.tsx
@@ -1,9 +1,46 @@
+import { Avatar, Typography } from "@mui/material";
 import { FlexBox } from "lib/ui-ux"
+import { chooseRandomColors, getInitialsByName } from "lib/utils";
+import { useMemo } from "react";
 
 export const EmailHistory = () => {
+    const history = [{ name: 'System Automation', content: 'SLA Violated - First Response Due' }];
+
     return (
         <FlexBox>
-            EmailHistory
+            {history.map((item, index) => <EmailHistoryCard key={index} name={item.name} content={item.content}/>)}
+        </FlexBox>
+    )
+}
+
+interface EmailHistoryCardProps {
+    name: string;
+    content: string
+}
+
+const EmailHistoryCard = (props: EmailHistoryCardProps) => {
+    const { name, content } = props;
+    const { backgroundColor, textColor } = useMemo(() => chooseRandomColors(getInitialsByName(name)), [name]);
+
+    return (
+        <FlexBox gap={'10px'}>
+            <Avatar sx={{
+                color: textColor,
+                bgcolor: backgroundColor,
+                width: '32px',
+                height: '32px',
+                fontSize: '13px',
+                fontWeight: 500
+            }}>
+                {getInitialsByName(name)}
+            </Avatar>
+            <FlexBox flexDirection="column" gap={'20px'}>
+                <FlexBox gap={'10px'}>
+                    <Typography variant="h6">{name}</Typography>
+                    <Typography variant="body3">made changes - 20 hrs ago</Typography>
+                </FlexBox>
+                <Typography variant="body2">{content}</Typography>
+            </FlexBox>
         </FlexBox>
     )
 }
