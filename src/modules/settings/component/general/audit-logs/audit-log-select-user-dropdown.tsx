c0bb6bf1332e4a3c64ef01c6358d14d3f8c8cc2d
Siddarth Menon <siddarth.menon@agilepoint.com>
2024-09-09 17:55:00 +0530

    few more changes

--- src/modules/settings/component/general/audit-logs/audit-log-select-user-dropdown.tsx
+++ src/modules/settings/component/general/audit-logs/audit-log-select-user-dropdown.tsx
@@ -28,7 +28,8 @@ interface IAuditLogSelectUserComponentProps extends IAuditLogSelectUserProps {
 
 const AuditLogSelectUserComponent = React.memo((props: IAuditLogSelectUserComponentProps) => {
     const { data, selectUserHandleChange, user } = props;
-
+    console.log('AuditLogSelectUserComponent');
+    
     return (
         <FormControl sx={{ width: '25%' }} size="small">
             <InputLabel id="demo-select-small-label">Users</InputLabel>
