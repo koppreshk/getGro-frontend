3d76f71b4e16f49b652451d13a8a1e49a94ed935
Siddharth1097 <koppresh@gmail.com>
2024-09-26 22:27:54 +0530

    fix: status anf tag issue fixes

---
+++ src/modules/settings/component/ticket-configurations/ticket-status/edit-status.tsx
@@ -0,0 +1,35 @@
+import { Edit } from "@mui/icons-material"
+import { CustomIconButton, DrawerExtended } from "lib/ui-ux"
+import { IGenericResponse } from "modules/settings/apis/templates/types"
+import { EditTicketStatusContainer } from "modules/settings/containers/ticket-status"
+import { useState, useCallback } from "react"
+
+interface EditStatusProps {
+    statusData: IGenericResponse[];
+    selectedData: IGenericResponse;
+}
+
+export const EditStatus = (props: EditStatusProps) => {
+    const { statusData, selectedData } = props;
+    const [showDrawer, setShowDrawer] = useState(false)
+
+    const toggleDrawer = useCallback(() => {
+        setShowDrawer((preValue) => !preValue);
+    }, []);
+
+    return (
+        <>
+            <CustomIconButton iconComponent={<Edit />} tooltipProps={{ title: "Edit Status", arrow: true }} onClick={toggleDrawer}/>
+            <DrawerExtended
+                open={showDrawer}
+                anchor="right"
+                width="500px"
+                header="View or Edit Ticket Status"
+                onRenderContent={() => (
+                    <EditTicketStatusContainer onSelectRowMetaData={selectedData} toggleDrawer={toggleDrawer} statusData={statusData} />
+                )}
+                onClose={toggleDrawer}
+            />
+        </>
+    )
+}
--- src/modules/settings/component/ticket-configurations/ticket-status/ticket-status-list.tsx
+++ src/modules/settings/component/ticket-configurations/ticket-status/ticket-status-list.tsx
@@ -1,38 +1,36 @@
-import { Edit } from "@mui/icons-material";
-import { Row, createColumnHelper } from "@tanstack/react-table";
-import { CustomIconButton, DrawerExtended, FlexBox } from "lib/ui-ux";
+import { createColumnHelper } from "@tanstack/react-table";
+import { FlexBox } from "lib/ui-ux";
 import { ConfigDataGrid } from "lib/ui-ux/configuration-data-grid";
 import { IGenericResponse } from "modules/settings/apis/ticket-status/types";
-import { DeleteTicketStatusContainer, EditTicketStatusContainer } from "modules/settings/containers/ticket-status";
-import { useCallback, useState } from "react";
+import { EditStatus } from "./edit-status";
 
 interface ITicketStatusListProps {
     statusData: IGenericResponse[] | undefined;
     isLoading: boolean;
 }
 
-const useColumns = () => {
+const useColumns = (statusData: IGenericResponse[]) => {
     const columnHelper = createColumnHelper<IGenericResponse>();
 
     const columns = [
         columnHelper.accessor("id", {
             id: 'id',
-            header: () => <span>Status ID</span>,
+            header: () => 'Status ID',
             cell: info => info.getValue(),
         }),
         columnHelper.accessor("name", {
             id: 'name',
-            header: () => <span>Status Name</span>,
+            header: () => 'Status Name',
             cell: info => info.getValue(),
+            minSize: 300
         }),
         columnHelper.display({
             id: 'actions',
-            header: () => <span>Actions</span>,
+            header: () => 'Actions',
             cell: ({ row: { original } }) => {
                 return (
                     <FlexBox flexDirection="row" gap="5px">
-                        <CustomIconButton iconComponent={<Edit />} tooltipProps={{ title: "Edit Status", arrow: true }} />
-                        <DeleteTicketStatusContainer id={original.id} />
+                        <EditStatus statusData={statusData} selectedData={original} />
                     </FlexBox>
                 )
             },
@@ -44,32 +42,11 @@ const useColumns = () => {
 
 export const TicketStatusList = (props: ITicketStatusListProps) => {
     const { isLoading, statusData } = props;
-    const columns = useColumns();
-    const [rowData, setRowData] = useState({});
-    const [showDrawer, setShowDrawer] = useState(false)
-
-    const toggleDrawer = useCallback(() => {
-        setShowDrawer((preValue) => !preValue);
-    }, []);
-
-    const onRowClick = useCallback((row: Row<IGenericResponse>) => {
-        setRowData(row.original);
-        toggleDrawer();
-    }, [toggleDrawer]);
+    const columns = useColumns(statusData!);
 
     return (
         <>
-            <ConfigDataGrid columns={columns} data={statusData!} hideTableControls isLoading={isLoading} onRowClick={onRowClick} />
-            <DrawerExtended
-                open={showDrawer}
-                anchor="right"
-                width="500px"
-                header="View or Edit Ticket Status"
-                onRenderContent={() => (
-                    <EditTicketStatusContainer onSelectRowMetaData={rowData as IGenericResponse} toggleDrawer={toggleDrawer} statusData={statusData}/>
-                )}
-                onClose={toggleDrawer}
-            />
+            <ConfigDataGrid columns={columns} data={statusData!} hideTableControls isLoading={isLoading} />
         </>
     )
 }
