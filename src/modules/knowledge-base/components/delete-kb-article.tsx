45c9533008b5ae101c1eff04fe1b408494cb7746
siddarth.menon@agilepoint.com <reachsiddarthmenon@gmail.com>
2024-10-12 18:55:41 +0530

    added localiztion

--- src/modules/knowledge-base/components/delete-kb-article.tsx
+++ src/modules/knowledge-base/components/delete-kb-article.tsx
@@ -1,21 +1,39 @@
 import { Delete } from "@mui/icons-material";
 import { useNotifications } from "lib";
-import { CustomIconButton } from "lib/ui-ux";
+import { CustomIconButton, NegativeActionDialog } from "lib/ui-ux";
 import { useDeleteKbArticle } from "../apis";
+import React from "react";
+import { useTranslation } from "react-i18next";
 
 export const DeleteKBArticle = (props: { id: number }) => {
-    const { mutateAsync } = useDeleteKbArticle();
+    const { mutateAsync, isLoading } = useDeleteKbArticle();
     const { showNotification } = useNotifications();
+    const [open, setOpen] = React.useState(false);
+    const { t } = useTranslation();
+
+    const toggleDeleteDialogBox = () => {
+        setOpen((prev) => !prev);
+    };
 
     const onDeleleHandler: React.MouseEventHandler<HTMLButtonElement> = (ev) => {
         ev.stopPropagation();
         mutateAsync({
             id: props.id
         })
-            .then(() => showNotification({ message: 'Article deleted successfully', type: 'success' }))
-            .catch(() => showNotification({ message: 'Failed to delete the Article', type: 'error' }))
+            .then(() => showNotification({ message: t('delete_article_success'), type: 'success' }))
+            .catch(() => showNotification({ message: t('delete_article_failed'), type: 'error' }))
     }
     return (
-        <CustomIconButton onClick={onDeleleHandler} iconComponent={<Delete />} tooltipProps={{ title: "Delete Article", arrow: true }} />
+        <>
+            <CustomIconButton onClick={toggleDeleteDialogBox} iconComponent={<Delete />} tooltipProps={{ title: t('delete_article'), arrow: true }} />
+            <NegativeActionDialog
+                open={open}
+                isLoading={isLoading}
+                content={t('confirm_article_delete_message')}
+                title={t('delete_article')}
+                negativeActionLabel={t('yes_delete')}
+                onNegativeActionClick={onDeleleHandler}
+                onClose={toggleDeleteDialogBox} />
+        </>
     )
 }
