8ad92c255073e32736ca5cf05656aa5cf0a890a1
Siddharth1097 <koppresh@gmail.com>
2024-10-19 14:43:30 +0530

    fix: added actual val

--- src/modules/settings/component/channel-configurations/facebook-configuration.tsx
+++ src/modules/settings/component/channel-configurations/facebook-configuration.tsx
@@ -123,7 +123,7 @@ export default function FacebookConfigurations() {
             } else {
                 console.log('User cancelled login or did not fully authorize.');
             }
-        }, { config_id: '<CONFIG_ID>' }); // Request email permission
+        }, { config_id: '1278485190260419' }); // Request email permission
     };
 
     const onCopy = () => {
