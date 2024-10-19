570f5ff5a57be03786e70da81c3f5a02d6ad235b
Siddharth1097 <koppresh@gmail.com>
2024-10-19 14:09:43 +0530

    feat: added native fb sdk

--- src/modules/settings/component/channel-configurations/facebook-configuration.tsx
+++ src/modules/settings/component/channel-configurations/facebook-configuration.tsx
@@ -1,11 +1,20 @@
-import { useState } from 'react';
-import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
+/* eslint-disable  */  // Disables all ESLint rules for this file
+
+import { useEffect, useState } from 'react';
 import { Button, Typography } from "@mui/material";
 import { Facebook } from '@mui/icons-material';
 import styled from 'styled-components';
 import { BreadCrumbs, FlexBox } from 'lib/ui-ux';
 import AssignmentOutlinedIcon from '@mui/icons-material/AssignmentOutlined';
 import { useNotifications } from 'lib';
+
+// Define Facebook SDK types for TypeScript
+declare global {
+    interface Window {
+        FB: any;
+        fbAsyncInit: () => void;
+    }
+}
 
 interface FacebookResponse {
     name: string;
@@ -78,13 +87,44 @@ export default function FacebookConfigurations() {
     const [facebookResponse, setFacebookResponse] = useState<null | FacebookResponse>(null);
     const { showNotification } = useNotifications();
 
-    const responseFacebook = (response: FacebookResponse) => {
-        if (response?.status === 'unknown') {
-            setFacebookResponse(null);
-            return;
-        }
-        setFacebookResponse(response);
-    }
+    useEffect(() => {
+        // Initialize the Facebook SDK
+        window.fbAsyncInit = function () {
+            window.FB.init({
+                appId: import.meta.env.VITE_FACEBOOK_APP_ID, // Replace with your Facebook App ID
+                cookie: true,
+                xfbml: true,
+                version: 'v10.0', // Use the latest version
+            });
+            window.FB.AppEvents.logPageView();
+        };
+
+        // Load the Facebook SDK script dynamically
+        (function (d, s, id) {
+            const fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            const js: any = d.createElement(s);
+            js.id = id;
+            js.src = 'https://connect.facebook.net/en_US/sdk.js';
+            fjs?.parentNode?.insertBefore(js, fjs);
+        })(document, 'script', 'facebook-jssdk');
+    }, []);
+
+    // Facebook login handler
+    const handleFBLogin = () => {
+        window.FB.login((response: any) => {
+            if (response.authResponse) {
+                console.log('Logged in!', response);
+
+                // Fetch user details like name and email
+                window.FB.api('/me', { fields: 'name, email' }, (userInfo: any) => {
+                    console.log('User info:', userInfo);
+                });
+            } else {
+                console.log('User cancelled login or did not fully authorize.');
+            }
+        }, { scope: 'email' }); // Request email permission
+    };
 
     const onCopy = () => {
         navigator.clipboard.writeText(JSON.stringify(facebookResponse!, null, 2))
@@ -104,15 +144,9 @@ export default function FacebookConfigurations() {
                         <FlexBox flexDirection='column'>
                             <Typography variant='h3'>Engage your customers, directly on facebook</Typography>
                             <Typography mb='20px' variant='body3'>Associate a facebook page with customer support and respond to customer comments and wall posts directly from getgro</Typography>
-                            <FacebookLogin
-                                appId={'1450143768984718'}
-                                fields="name,email,picture"
-                                render={renderProps => (
-                                    <StyledButton startIcon={<Facebook />} sx={{ width: '300px' }} variant='contained' onClick={renderProps.onClick}>
-                                        Login with facebook
-                                    </StyledButton>
-                                )}
-                                callback={responseFacebook} />
+                            <StyledButton startIcon={<Facebook />} sx={{ width: '300px' }} variant='contained' onClick={handleFBLogin}>
+                                Login with facebook
+                            </StyledButton>
                         </FlexBox>
                     </FlexBox>
                     :
@@ -148,3 +182,5 @@ export default function FacebookConfigurations() {
         </>
     )
 }
+
+/* eslint-enable */
