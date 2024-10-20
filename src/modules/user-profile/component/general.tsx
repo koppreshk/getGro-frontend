0a4e34177a07749eb4a015967ee4eaef61bb638a
Siddharth1097 <koppresh@gmail.com>
2024-10-20 13:57:08 +0530

    fix: added more ux

--- src/modules/user-profile/component/general.tsx
+++ src/modules/user-profile/component/general.tsx
@@ -1,35 +1,147 @@
-import { Button, Typography } from "@mui/material"
-import { SelectFieldWithLabel, TextboxFieldWithLabel } from "lib/form-fields"
-import { FlexBox } from "lib/ui-ux";
+import { useState } from "react";
 import { useForm, FormProvider } from "react-hook-form";
 import { useTranslation } from "react-i18next";
+import { Cancel, Edit, CheckCircle } from "@mui/icons-material";
+import { Button, IconButton, Typography } from "@mui/material"
+import { SelectFieldWithLabel, TextboxField } from "lib/form-fields"
+import { useAppSelector } from "lib/hooks";
+import { FlexBox } from "lib/ui-ux";
+
+interface IGeneralTabFormFeilds {
+    fullName: string;
+    displayName: string;
+    ticketLayoutView: string;
+    defaultTicketView: string;
+    ticketPageCount: string;
+}
 
 export const General = () => {
-    const formMethods = useForm();
+    const config = useAppSelector((state) => state.core.config);
+    const formMethods = useForm<IGeneralTabFormFeilds>({
+        defaultValues: {
+            fullName: config?.user_details.first_name,
+            displayName: config?.user_details.display_name,
+            defaultTicketView: 'all-tickets',
+            ticketLayoutView: 'card-view',
+            ticketPageCount: '10'
+        },
+    });
+
     const { t } = useTranslation();
 
     return (
         <FormProvider {...formMethods}>
-            <FlexBox flexDirection="column" gap="20px" width="60%" padding="20px">
-                <FlexBox flexDirection="column" gap="10px" width="100%">
-                    <Typography variant="h5">Basic Info</Typography>
-                    <FlexBox gap={'20px'} width="100%" flexDirection="column">
-                        <TextboxFieldWithLabel name="fullName" width="40%" label={t('full_name')} size="small" type="text" />
-                        <TextboxFieldWithLabel name="displayName" width="40%" label={t('display_name')} size="small" type="text" />
+            <FlexBox flexDirection="column" justifyContent="space-between" padding="20px" height="calc(100% - 49px)">
+                <FlexBox flexDirection="column" gap="40px" width="60%">
+                    <FlexBox flexDirection="column" gap="10px" width="100%">
+                        <Typography variant="h5">Basic Info</Typography>
+                        <FlexBox gap={'10px'} width="100%" flexDirection="column">
+                            <EditUserDetails label={t('full_name')} formFieldName="fullName" value={config?.user_details.first_name} />
+                            <EditUserDetails label={t('display_name')} formFieldName="displayName" value={config?.user_details.display_name} />
+                        </FlexBox>
                     </FlexBox>
-                </FlexBox>
-                <FlexBox flexDirection="column" gap="10px" >
-                    <Typography variant="h5">Ticket Settings</Typography>
                     <FlexBox flexDirection="column" gap="10px" >
-                        <SelectFieldWithLabel name="ticketLayoutView" sx={{ width: '300px' }} label={t('ticket_layout_view')} size="small" menuOptions={[{ key: 'card-view', value: t('card_view') }, { key: 'grid-view', value: t('grid_view') }]} />
-                        <SelectFieldWithLabel name="defaultTicketView" sx={{ width: '300px' }} label={t('default_ticket_view')} size="small" menuOptions={[{ key: 'card-view', value: 'Card View' }]} />
-                        <SelectFieldWithLabel name="defaultTicketView" sx={{ width: '300px' }} label={t('ticket_page_count')} size="small" menuOptions={[{ key: 'card-view', value: 'Card View' }]} />
+                        <Typography variant="h5">Ticket Settings</Typography>
+                        <FlexBox flexDirection="column" gap="10px" >
+                            <SelectFieldWithLabel name="ticketLayoutView" sx={{ width: '300px' }} label={t('ticket_layout_view')} size="small" menuOptions={[{ key: 'card-view', value: t('card_view') }, { key: 'grid-view', value: t('grid_view') }]} />
+                            <SelectFieldWithLabel
+                                name="defaultTicketView" sx={{ width: '300px' }}
+                                label={t('default_ticket_view')} size="small"
+                                menuOptions={[
+                                    {
+                                        value: t('all_tickets'),
+                                        key: 'all-tickets',
+                                    },
+                                    {
+                                        value: t('all_pending'),
+                                        key: 'all-pending',
+                                    },
+                                    {
+                                        value: t('all_resolved'),
+                                        key: 'all-resolved',
+                                    },
+                                    {
+                                        value: t('all_closed'),
+                                        key: 'all-closed',
+                                    },
+                                    {
+                                        value: t('my_pending'),
+                                        key: 'my-pending',
+                                    },
+                                    {
+                                        value: t('my_resolved'),
+                                        key: 'my-resolved',
+                                    },
+                                    {
+                                        value: t('my_closed'),
+                                        key: 'my-closed',
+                                    }
+                                ]} />
+                            <SelectFieldWithLabel
+                                name="ticketPageCount" sx={{ width: '300px' }}
+                                label={t('ticket_page_count')} size="small"
+                                menuOptions={[
+                                    { key: '10', value: '10' },
+                                    { key: '20', value: '20' },
+                                    { key: '30', value: '30' },
+                                    { key: '40', value: '40' },
+                                    { key: '50', value: '50' },
+                                ]} />
+                        </FlexBox>
                     </FlexBox>
                 </FlexBox>
-                <Button variant="contained" type="submit" sx={{ width: '50%' }}>
-                    Save
-                </Button>
+                <FlexBox gap={'10px'} width={'50%'}>
+                    <Button variant="outlined" sx={{ width: '50%' }} onClick={() => formMethods.reset()}>
+                        {t('reset')}
+                    </Button>
+                    <Button variant="contained" type="submit" sx={{ width: '50%' }}>
+                        {t('save')}
+                    </Button>
+                </FlexBox>
             </FlexBox>
         </FormProvider>
+    )
+}
+
+interface EditUserDetailsProps {
+    label: string;
+    value?: string;
+    formFieldName: string;
+}
+
+const EditUserDetails = (props: EditUserDetailsProps) => {
+    const { label, value, formFieldName } = props;
+    const [isInEditMode, setEditMode] = useState(false);
+
+    const toggleEditMode = () => setEditMode((pre) => !pre);
+
+    return (
+        <FlexBox gap={'10px'} alignItems="center">
+            <Typography variant="body2" sx={{ minWidth: '200px' }}>{label}</Typography>
+            {!isInEditMode
+                ?
+                (
+                    <FlexBox alignItems="center" gap={'4px'}>
+                        <Typography variant="body2">{value}</Typography>
+                        <IconButton onClick={toggleEditMode}>
+                            <Edit />
+                        </IconButton>
+                    </FlexBox>
+                )
+                :
+                (
+
+                    <FlexBox alignItems="center" gap={'4px'}>
+                        <TextboxField name={formFieldName} width="40%" label={`Modify ${label}`} size="small" type="text" />
+                        <IconButton onClick={toggleEditMode}>
+                            <Cancel color="error" />
+                        </IconButton>
+                        <IconButton onClick={toggleEditMode}>
+                            <CheckCircle color="success" />
+                        </IconButton>
+                    </FlexBox>
+                )
+            }
+        </FlexBox>
     )
 }
