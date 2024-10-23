b5c39673024941ceedec86ccbba9a6071e679af7
Siddharth1097 <koppresh@gmail.com>
2024-10-23 20:14:10 +0530

    fix: sending subdomain value

--- src/modules/login/apis/reset-password.ts
+++ src/modules/login/apis/reset-password.ts
@@ -11,9 +11,12 @@ interface IResetPassowordArgs {
 
 export const useResetPassword = () => {
     const { postData } = useServiceClient();
+    const subDomain = import.meta.env.VITE_SUB_DOMAIN ?? new URL(location.origin).href; //Keeping env values incase of overiding from local
 
     const onResetPassword = React.useCallback((args: IResetPassowordArgs) =>
-        postData(LoginEndPoint.RESET_PASSWORD, args).then((res) => res.json()), [postData]);
+        postData(LoginEndPoint.RESET_PASSWORD, args, {
+            'sub-domain': subDomain
+        }).then((res) => res.json()), [postData, subDomain]);
 
     return useMutation({
         mutationFn: onResetPassword,
