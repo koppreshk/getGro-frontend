d6c6a15ae9aea543231bcfa222edf5838a47e655
koppreshk <koppresh@gmail.com>
2024-11-24 09:17:36 +0530

    fix: added glogin in add-email

--- src/modules/settings/component/channel-configurations/email/add-email.tsx
+++ src/modules/settings/component/channel-configurations/email/add-email.tsx
@@ -3,6 +3,7 @@ import { SwitchField, TextboxField } from "lib/form-fields";
 import { CancelButton, FlexBox, GridLayout, HorizontalSeparator, LoadingButton } from "lib/ui-ux";
 import { useFormContext } from "react-hook-form";
 import { useTranslation } from "react-i18next";
+import { GoogleOAuthProvider, GoogleLogin, CredentialResponse } from '@react-oauth/google';
 import { useNavigate } from "react-router-dom";
 
 export interface IAddEmailConfigFormFields {
@@ -23,32 +24,52 @@ export const AddEmail = (props: IAddEmailProps) => {
     const navigate = useNavigate();
     const { t } = useTranslation();
 
+    const handleSuccess = (response: CredentialResponse) => {
+        console.log('Login successful!', response);
+        if (response.credential) {
+            // Use the credential token to authenticate the user or call backend API
+            console.log('Token:', response.credential);
+        }
+    };
+
+    const handleError = () => {
+        console.log('Login Failed');
+    };
+
     return (
-        <FlexBox padding="20px" flexDirection="column" gap={'20px'} >
-            <FlexBox flexDirection="column" gap='10px'>
-                <Typography variant="h5">{t('email_configuration')}</Typography>
-                <HorizontalSeparator />
-            </FlexBox>
-            <GridLayout $gridGap={'50px'} $alignItems="center" $gridTemplateColumns={'105px auto'}>
-                <Typography variant="h6">{t('display_name')}</Typography>
-                <FlexBox flexDirection="column" gap={'5px'}>
-                    <TextboxField name="displayName" sx={{ width: '350px' }} size="small" rules={{ required: t('display_name_validation') }} />
-                    <Typography variant="body3">{t('display_name_helper')}</Typography>
+        <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID}>
+            <FlexBox padding="20px" flexDirection="column" gap={'20px'} >
+                <FlexBox flexDirection="column" gap='10px'>
+                    <Typography variant="h5">{t('email_configuration')}</Typography>
+                    <HorizontalSeparator />
                 </FlexBox>
-            </GridLayout>
-            <GridLayout $gridGap={'50px'} $alignItems="center" $gridTemplateColumns={'105px auto'}>
-                <Typography variant="h6">{t('email_address')}</Typography>
-                <TextboxField name="emailAddress" disabled={formType === 'EDIT'} type="email" sx={{ width: '350px' }} size="small" rules={{ required: t('email_address_required') }} />
-            </GridLayout>
-            <GridLayout $gridGap={'50px'} $alignItems="center" $gridTemplateColumns={'105px auto'}>
-                <Typography variant="h6">{t('email_active')}</Typography>
-                <SwitchField name="isActive" />
-            </GridLayout>
-            <HorizontalSeparator />
-            <FlexBox gap={'50px'}>
-                <CancelButton sx={{ width: '100px' }} onClick={() => navigate(-1)} />
-                <LoadingButton variant="contained" sx={{ width: '150px' }} onClick={handleSubmit(onSubmit)} isLoading={mutationLoading}>{formType === 'ADD' ? t('add') : t('edit')}</LoadingButton>
+                <GridLayout $gridGap={'50px'} $alignItems="center" $gridTemplateColumns={'105px auto'}>
+                    <Typography variant="h6">{t('display_name')}</Typography>
+                    <FlexBox flexDirection="column" gap={'5px'}>
+                        <TextboxField name="displayName" sx={{ width: '350px' }} size="small" rules={{ required: t('display_name_validation') }} />
+                        <Typography variant="body3">{t('display_name_helper')}</Typography>
+                    </FlexBox>
+                </GridLayout>
+                <GridLayout $gridGap={'50px'} $alignItems="center" $gridTemplateColumns={'105px auto'}>
+                    <Typography variant="h6">{t('email_address')}</Typography>
+                    <TextboxField name="emailAddress" disabled={formType === 'EDIT'} type="email" sx={{ width: '350px' }} size="small" rules={{ required: t('email_address_required') }} />
+                </GridLayout>
+                <GridLayout $gridGap={'50px'} $alignItems="center" $gridTemplateColumns={'105px auto'}>
+                    <Typography variant="h6">{t('email_active')}</Typography>
+                    <SwitchField name="isActive" />
+                </GridLayout>
+                <GridLayout $gridTemplateColumns={'105px auto'}>
+                    <GoogleLogin
+                        onSuccess={handleSuccess}
+                        onError={handleError}
+                    />
+                </GridLayout>
+                <HorizontalSeparator />
+                <FlexBox gap={'50px'}>
+                    <CancelButton sx={{ width: '100px' }} onClick={() => navigate(-1)} />
+                    <LoadingButton variant="contained" sx={{ width: '150px' }} onClick={handleSubmit(onSubmit)} isLoading={mutationLoading}>{formType === 'ADD' ? t('add') : t('edit')}</LoadingButton>
+                </FlexBox>
             </FlexBox>
-        </FlexBox>
+        </GoogleOAuthProvider>
     )
 }
