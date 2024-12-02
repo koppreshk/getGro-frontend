46a0588b8cb5d1c83eb37ed30c5a10a1aab43a8c
koppreshk <koppresh@gmail.com>
2024-12-02 21:06:06 +0530

    fixed merge changes

--- src/modules/settings/component/channel-configurations/web-forms/preview-form-external.tsx
+++ src/modules/settings/component/channel-configurations/web-forms/preview-form-external.tsx
@@ -1,16 +1,15 @@
-import { FormProvider, useForm } from "react-hook-form"
-import { PreviewForm } from "./preview-form";
-import { Typography } from "@mui/material";
+import { FormProvider, useForm } from 'react-hook-form';
+
+import { PreviewForm } from './preview-form';
 
 export const ExternalPreviewForm = () => {
-    const form = useForm();
-    const formTitle = 'Contact us'; //should come from api
-    const formDescription = 'abc';
+  const form = useForm();
+  const formTitle = 'Contact us'; //should come from api
+  const formDescription = 'abc';
 
-    return (
-        <FormProvider {...form}>
-            <Typography>External</Typography>
-            <PreviewForm formTitle={formTitle} formDescription={formDescription} />
-        </FormProvider>
-    )
-}
+  return (
+    <FormProvider {...form}>
+      <PreviewForm formTitle={formTitle} formDescription={formDescription} />
+    </FormProvider>
+  );
+};
