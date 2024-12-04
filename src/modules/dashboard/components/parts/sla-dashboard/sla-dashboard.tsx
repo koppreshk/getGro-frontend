8b9392002a3f73e3997994d406fc548fa64ee711
siddarthmenon97 <reachsiddarthmenon@gmail.com>
2024-12-04 19:34:31 +0530

    SLA dashboard localization added

--- src/modules/dashboard/components/parts/sla-dashboard/sla-dashboard.tsx
+++ src/modules/dashboard/components/parts/sla-dashboard/sla-dashboard.tsx
@@ -1,28 +1,39 @@
-import React from "react";
-import { FlexBox } from "lib/ui-ux"
-import { SLAmetricsChart, TicketsBreached } from ".";
-import { DashboardDateRangePicker } from "../dashboard-date-range-picker";
-import { DateRange } from "@matharumanpreet00/react-daterange-picker";
-import { ISLAValues } from "modules/dashboard/apis";
+import { DateRange } from '@matharumanpreet00/react-daterange-picker';
+import { FlexBox } from 'lib/ui-ux';
+import { ISLAValues } from 'modules/dashboard/apis';
+import React from 'react';
+
+import { DashboardDateRangePicker } from '../dashboard-date-range-picker';
+
+import { SLAmetricsChart, TicketsBreached } from '.';
 
 interface ISLADashboardProps {
-    data: ISLAValues;
-    dateRange: DateRange;
-    setDateRange: React.Dispatch<React.SetStateAction<DateRange>>;
+  data: ISLAValues;
+  dateRange: DateRange;
+  setDateRange: React.Dispatch<React.SetStateAction<DateRange>>;
 }
 
 export const SLADashboard = (props: ISLADashboardProps) => {
-    const { data, dateRange, setDateRange } = props;
+  const { data, dateRange, setDateRange } = props;
 
-    return (
-        <>
-            <FlexBox flexDirection="column" gap="20px" height="100%" width="100%" padding="25px 25px">
-                <FlexBox justifyContent="flex-end" alignItems="center">
-                    <DashboardDateRangePicker dateRange={dateRange} setDateRange={setDateRange} />
-                </FlexBox>
-                <TicketsBreached data={data} />
-                <SLAmetricsChart dateRange={dateRange} />
-            </FlexBox>
-        </>
-    )
-}
+  return (
+    <>
+      <FlexBox
+        flexDirection="column"
+        gap="20px"
+        height="100%"
+        width="100%"
+        padding="25px 25px"
+      >
+        <FlexBox justifyContent="flex-end" alignItems="center">
+          <DashboardDateRangePicker
+            dateRange={dateRange}
+            setDateRange={setDateRange}
+          />
+        </FlexBox>
+        <TicketsBreached data={data} />
+        <SLAmetricsChart dateRange={dateRange} />
+      </FlexBox>
+    </>
+  );
+};
--- src/modules/dashboard/components/parts/sla-dashboard/tickets-breached.tsx
+++ src/modules/dashboard/components/parts/sla-dashboard/tickets-breached.tsx
@@ -1,143 +1,204 @@
-import { Typography } from "@mui/material";
-import { FlexBox, GridLayout } from "lib/ui-ux";
-import styled, { useTheme } from "styled-components";
-import ReactApexChart from "react-apexcharts";
-import { ApexOptions } from "apexcharts";
-import { ISLAValues } from "modules/dashboard/apis";
+import { Typography } from '@mui/material';
+import { ApexOptions } from 'apexcharts';
+import { FlexBox, GridLayout } from 'lib/ui-ux';
+import { ISLAValues } from 'modules/dashboard/apis';
+import ReactApexChart from 'react-apexcharts';
+import { useTranslation } from 'react-i18next';
+import styled, { useTheme } from 'styled-components';
 
 const StyledLayout = styled(GridLayout)`
-    background: ${({ theme }) => theme.pallete.white};
-    border-radius: ${({ theme }) => theme.semantics.borderRadius.md};
+  background: ${({ theme }) => theme.pallete.white};
+  border-radius: ${({ theme }) => theme.semantics.borderRadius.md};
 `;
 
 const DataGridLayout = styled(GridLayout)`
-
-    .stats-wrapper:last-child {
-        border-right: none;
-    }
-    .stats-wrapper:first-child {
-        padding: 0;
-    }
+  .stats-wrapper:last-child {
+    border-right: none;
+  }
+  .stats-wrapper:first-child {
+    padding: 0;
+  }
 `;
 
 const StatsWrapper = styled(FlexBox)`
-    border-right: ${({ theme }) => theme.semantics.standardBorder};
-    padding-left: 30px;
+  border-right: ${({ theme }) => theme.semantics.standardBorder};
+  padding-left: 30px;
 `;
 
 export const TicketsBreached = (props: { data: ISLAValues }) => {
-    const { data } = props;
-    const { pallete } = useTheme();
-    const { unique_ticket_count, sla_breach_percentage, sla_breached_count, sla_achieved_count, sla_achieved_percentage } = data.sla_applied_tickets;
+  const { data } = props;
+  const { pallete } = useTheme();
+  const { t } = useTranslation();
+  const {
+    unique_ticket_count,
+    sla_breach_percentage,
+    sla_breached_count,
+    sla_achieved_count,
+    sla_achieved_percentage,
+  } = data.sla_applied_tickets;
 
-    return (
-        <GridLayout $gridTemplateColumns="repeat(2, 1fr)" $gridGap="20px">
+  return (
+    <GridLayout $gridTemplateColumns="repeat(2, 1fr)" $gridGap="20px">
+      <StyledLayout $gridTemplateColumns="70% 30%" $padding="20px">
+        <FlexBox flexDirection="column" gap="14px">
+          <FlexBox gap="6px">
+            <Typography variant="h5" sx={{ color: pallete.grayVariant3 }}>
+              {t('sla_applied_tickets')}
+            </Typography>
+            <Typography variant="body3" sx={{ color: pallete.grayNeutral }}>
+              ({unique_ticket_count} {t('unique_tickets')})
+            </Typography>
+          </FlexBox>
 
-            <StyledLayout $gridTemplateColumns="70% 30%" $padding="20px">
-                <FlexBox flexDirection="column" gap="14px">
-                    <FlexBox gap="6px">
-                        <Typography variant="h5" sx={{ color: pallete.grayVariant3 }} >SLA Applied Tickets</Typography>
-                        <Typography variant="body3" sx={{ color: pallete.grayNeutral }}>({unique_ticket_count} Unique Tickets)</Typography>
-                    </FlexBox>
+          <DataGridLayout $gridTemplateColumns="repeat(2, 1fr)">
+            <StatsWrapper
+              gap="8px"
+              flexDirection="column"
+              className="stats-wrapper"
+            >
+              <Typography variant="body2">{t('sla_breached')}</Typography>
+              <Typography variant="h2">
+                {Math.round(sla_breach_percentage) || 0}%
+              </Typography>
+              <Typography variant="body3">
+                {sla_breached_count} {t('tickets')}
+              </Typography>
+            </StatsWrapper>
+            <StatsWrapper
+              gap="8px"
+              flexDirection="column"
+              className="stats-wrapper"
+            >
+              <Typography variant="body2">{t('sla_achieved')}</Typography>
+              <Typography variant="h2">
+                {Math.round(sla_achieved_percentage) || 0}%
+              </Typography>
+              <Typography variant="body3">
+                {sla_achieved_count} {t('tickets')}
+              </Typography>
+            </StatsWrapper>
+          </DataGridLayout>
+        </FlexBox>
 
-                    <DataGridLayout $gridTemplateColumns="repeat(2, 1fr)">
-                        <StatsWrapper gap="8px" flexDirection="column" className="stats-wrapper">
-                            <Typography variant="body2">SLA Breached</Typography >
-                            <Typography variant="h2">{Math.round(sla_breach_percentage) || 0}%</Typography>
-                            <Typography variant="body3">{sla_breached_count} Tickets</Typography>
-                        </StatsWrapper>
-                        <StatsWrapper gap="8px" flexDirection="column" className="stats-wrapper">
-                            <Typography variant="body2">SLA Achieved</Typography>
-                            <Typography variant="h2">{Math.round(sla_achieved_percentage) || 0}%</Typography>
-                            <Typography variant="body3">{sla_achieved_count} Tickets</Typography>
-                        </StatsWrapper>
-                    </DataGridLayout>
+        <TicketsBreachedChart
+          breachedPercentage={Math.round(sla_breach_percentage)}
+        />
+      </StyledLayout>
 
-                </FlexBox>
+      <StyledLayout $gridTemplateColumns="60% 40%" $padding="20px">
+        <FlexBox flexDirection="column" gap="14px">
+          <FlexBox gap="6px">
+            <Typography variant="h5" sx={{ color: pallete.grayVariant3 }}>
+              {t('sla_breached')}
+            </Typography>
+            <Typography variant="body3" sx={{ color: pallete.grayNeutral }}>
+              ({data.sla_breaches.unique_ticket_count} {t('unique_tickets')})
+            </Typography>
+          </FlexBox>
 
-                <TicketsBreachedChart breachedPercentage={Math.round(sla_breach_percentage)} />
-
-            </StyledLayout>
-
-            <StyledLayout $gridTemplateColumns="60% 40%" $padding="20px">
-                <FlexBox flexDirection="column" gap="14px">
-                    <FlexBox gap="6px">
-                        <Typography variant="h5" sx={{ color: pallete.grayVariant3 }} >SLA Breaches</Typography>
-                        <Typography variant="body3" sx={{ color: pallete.grayNeutral }}>({data.sla_breaches.unique_ticket_count} Unique Tickets)</Typography>
-                    </FlexBox>
-
-                    <DataGridLayout $gridTemplateColumns="repeat(2, 1fr)">
-                        <StatsWrapper gap="8px" flexDirection="column" className="stats-wrapper">
-                            <Typography variant="body2">Response Breaches</Typography >
-                            <Typography variant="h2">{Math.round(data.sla_breaches.response_breach_percentage) || 0}%</Typography>
-                            <Typography variant="body3">{data.sla_breaches.response_breached_count} Times</Typography>
-                        </StatsWrapper>
-                        <StatsWrapper gap="8px" flexDirection="column" className="stats-wrapper">
-                            <Typography variant="body2">Resolution Breaches</Typography>
-                            <Typography variant="h2">{Math.round(data.sla_breaches.resolution_breach_percentage) || 0}%</Typography>
-                            <Typography variant="body3">{data.sla_breaches.resolution_breached_count} Times</Typography>
-                        </StatsWrapper>
-                    </DataGridLayout>
-
-                </FlexBox>
-                <FlexBox alignItems='center' height='100%'>
-                    <SLABreachedChart respBreachPercent={Math.round(data.sla_breaches.response_breach_percentage) || 0} reslnBreachPercent={Math.round(data.sla_breaches.resolution_breach_percentage) || 0} />
-                </FlexBox>
-            </StyledLayout>
-        </GridLayout>
-    )
-}
+          <DataGridLayout $gridTemplateColumns="repeat(2, 1fr)">
+            <StatsWrapper
+              gap="8px"
+              flexDirection="column"
+              className="stats-wrapper"
+            >
+              <Typography variant="body2">{t('response_breaches')}</Typography>
+              <Typography variant="h2">
+                {Math.round(data.sla_breaches.response_breach_percentage) || 0}%
+              </Typography>
+              <Typography variant="body3">
+                {data.sla_breaches.response_breached_count} {t('times_count')}
+              </Typography>
+            </StatsWrapper>
+            <StatsWrapper
+              gap="8px"
+              flexDirection="column"
+              className="stats-wrapper"
+            >
+              <Typography variant="body2">
+                {t('resolution_breaches')}
+              </Typography>
+              <Typography variant="h2">
+                {Math.round(data.sla_breaches.resolution_breach_percentage) ||
+                  0}
+                %
+              </Typography>
+              <Typography variant="body3">
+                {data.sla_breaches.resolution_breached_count} {t('times_count')}
+              </Typography>
+            </StatsWrapper>
+          </DataGridLayout>
+        </FlexBox>
+        <FlexBox alignItems="center" height="100%">
+          <SLABreachedChart
+            respBreachPercent={
+              Math.round(data.sla_breaches.response_breach_percentage) || 0
+            }
+            reslnBreachPercent={
+              Math.round(data.sla_breaches.resolution_breach_percentage) || 0
+            }
+          />
+        </FlexBox>
+      </StyledLayout>
+    </GridLayout>
+  );
+};
 
 const TicketsBreachedChart = (prop: { breachedPercentage: number }) => {
+  const data = {
+    series: [prop.breachedPercentage],
+    options: {
+      chart: {
+        type: 'radialBar',
+      },
+      plotOptions: {
+        radialBar: {
+          hollow: {
+            size: '60%',
+          },
+        },
+      },
+      labels: ['Breached'],
+      colors: ['#6a69f6'],
+    } as ApexOptions,
+  };
 
-    const data = {
-        series: [prop.breachedPercentage],
-        options: {
-            chart: {
-                type: 'radialBar',
-            },
-            plotOptions: {
-                radialBar: {
-                    hollow: {
-                        size: '60%'
-                    }
-                }
-            },
-            labels: ['Breached'],
-            colors: ['#6a69f6']
-        } as ApexOptions
-    };
+  return (
+    <ReactApexChart
+      options={data.options}
+      series={data.series}
+      type="radialBar"
+    />
+  );
+};
 
+const SLABreachedChart = (props: {
+  respBreachPercent: number;
+  reslnBreachPercent: number;
+}) => {
+  const { t } = useTranslation();
+  const data = {
+    series: Object.values(props),
+    options: {
+      chart: {
+        fontFamily: 'Poppins',
+        type: 'donut',
+      },
+      labels: [t('response'), t('resolution')],
+      dataLabels: {
+        style: {
+          fontSize: '10px',
+        },
+      },
+      plotOptions: {
+        pie: {
+          customScale: 1.1,
+        },
+      },
+    } as ApexOptions,
+  };
 
-    return (
-        <ReactApexChart options={data.options} series={data.series} type="radialBar" />
-    )
-}
-
-const SLABreachedChart = (props: { respBreachPercent: number, reslnBreachPercent: number }) => {
-
-    const data = {  
-        series: Object.values(props),
-        options: {
-            chart: {
-                fontFamily: 'Poppins',
-                type: 'donut',
-            },
-            labels: ['Response', 'Resolution'],
-            dataLabels: {
-                style: {
-                    fontSize: '10px'
-                }
-            },
-            plotOptions: {
-                pie: {
-                    customScale: 1.1    
-                }
-            }
-        } as ApexOptions
-    };
-
-    return (
-        <ReactApexChart options={data.options} series={data.series} type="donut" />
-    )
-}
+  return (
+    <ReactApexChart options={data.options} series={data.series} type="donut" />
+  );
+};
