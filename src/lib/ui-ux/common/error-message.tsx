ca015246670b12adee9ce2f0a82cbe325a2d93c3
koppreshk <koppresh@gmail.com>
2024-12-15 13:08:12 +0530

    fix: added 500 status code error message

--- src/lib/ui-ux/common/error-message.tsx
+++ src/lib/ui-ux/common/error-message.tsx
@@ -1,36 +1,50 @@
 /// <reference types="vite-plugin-svgr/client" />
 
-import { Suspense, lazy, useMemo } from 'react';
 import { Typography } from '@mui/material';
+import { Suspense, lazy, useMemo } from 'react';
+
 import { FlexBox } from '..';
 
-const ErrorIllustration = lazy(() => import('../../../../src/assets/svg/api-error.svg?react'));
+const ErrorIllustration = lazy(
+  () => import('../../../../src/assets/svg/api-error.svg?react')
+);
 export const ErrorMessage = (props: { statusCode?: string }) => {
-    const { statusCode } = props;
+  const { statusCode } = props;
 
-    const parsedStatusCode = Number(statusCode?.split(':')[1]);
+  const parsedStatusCode = Number(statusCode?.split(':')[1]);
 
-    const parsedMessage = useMemo(() => {
-        switch (parsedStatusCode) {
-            case 401:
-                return 'The content is only visible to authorized user'
-            case 404:
-                return 'The content you are trying to find does not exist'
-            default: return 'Unable to fetch this content'
-        }
-    }, [parsedStatusCode])
+  const parsedMessage = useMemo(() => {
+    switch (parsedStatusCode) {
+      case 401:
+        return 'The content is only visible to authorized user';
+      case 404:
+        return 'The content you are trying to find does not exist';
+      case 500:
+        return 'Internal server error, please try after some time';
+      default:
+        return 'Unable to fetch this content';
+    }
+  }, [parsedStatusCode]);
 
-    return (
-        <Suspense fallback={<span>Loading illustration...</span>}>
-            <FlexBox width='100%' height='100%' flexDirection='column' alignItems='center' justifyContent='center'>
-                <ErrorIllustration width="60%" height="60%" />
-                {parsedStatusCode ?
-                    <FlexBox gap={'4px'}>
-                        <Typography sx={{ color: 'red' }}>{parsedStatusCode}: </Typography>
-                        <Typography sx={{ color: 'red' }}>{parsedMessage}</Typography>
-                    </FlexBox>
-                    : <Typography sx={{ color: 'red' }}>{parsedMessage}</Typography>}
-            </FlexBox>
-        </Suspense>
-    )
-}
+  return (
+    <Suspense fallback={<span>Loading illustration...</span>}>
+      <FlexBox
+        width="100%"
+        height="100%"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <ErrorIllustration width="60%" height="60%" />
+        {parsedStatusCode ? (
+          <FlexBox gap={'4px'}>
+            <Typography sx={{ color: 'red' }}>{parsedStatusCode}: </Typography>
+            <Typography sx={{ color: 'red' }}>{parsedMessage}</Typography>
+          </FlexBox>
+        ) : (
+          <Typography sx={{ color: 'red' }}>{parsedMessage}</Typography>
+        )}
+      </FlexBox>
+    </Suspense>
+  );
+};
