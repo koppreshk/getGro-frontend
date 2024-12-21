0f0eb9e546275f229434ef66b095c2f04f091eb6
koppreshk <koppresh@gmail.com>
2024-12-21 12:15:54 +0530

    fix: added error block

--- src/modules/customers/containers/all-customers-container.tsx
+++ src/modules/customers/containers/all-customers-container.tsx
@@ -1,14 +1,20 @@
+import { ErrorMessage } from 'lib/ui-ux';
+
 import { useFetchAllCustomers } from '../apis/fetch-all-customers';
 import { AllCustomers } from '../components/all-customers';
 
 export const AllCustomersContainer = () => {
-  const { isLoading, data } = useFetchAllCustomers();
+  const { isLoading, data, error } = useFetchAllCustomers();
 
-  return (
-    <AllCustomers
-      data={data?.data}
-      totalPages={data?.total_pages}
-      isLoading={isLoading}
-    />
-  );
+  if (data || isLoading) {
+    return (
+      <AllCustomers
+        data={data?.data}
+        totalPages={data?.total_pages}
+        isLoading={isLoading}
+      />
+    );
+  }
+
+  return <ErrorMessage statusCode={error?.message} />;
 };
