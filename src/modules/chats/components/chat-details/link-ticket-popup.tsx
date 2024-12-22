7e180e5bf8eb2a33ba6f053aa024ec17f5863e9b
koppreshk <koppresh@gmail.com>
2024-12-22 11:54:13 +0530

    fix: added localization changes

--- src/modules/chats/components/chat-details/link-ticket-popup.tsx
+++ src/modules/chats/components/chat-details/link-ticket-popup.tsx
@@ -11,6 +11,7 @@ import {
   TextField,
   debounce,
 } from '@mui/material';
+import { t } from 'i18next';
 import { useNotifications } from 'lib';
 import { StyledErrorMessage } from 'lib/form-fields';
 import { useAppSelector } from 'lib/hooks';
@@ -26,7 +27,6 @@ import {
   useForm,
   useFormContext,
 } from 'react-hook-form';
-import { useTranslation } from 'react-i18next';
 
 interface LinkTicketDialogProps {
   openLinkTicketDialog: boolean;
@@ -42,9 +42,100 @@ interface ISearchFormFields {
   }[];
 }
 
+const SearchTickets = (props: {
+  isLoading: boolean;
+  data?: ISearchTickets;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+}) => {
+  const { data, isLoading, onChange } = props;
+  const {
+    formState: { errors },
+    control,
+  } = useFormContext();
+  const hasError = get(errors, 'searchTickets') !== undefined;
+
+  const validateField = (val: []) => {
+    if (val.length <= 0) {
+      return t('merge_ticket_validation');
+    }
+  };
+
+  const debouncedChange = debounce(onChange, 500);
+
+  return (
+    <FlexBox flexDirection="column" gap={'5px'} width="400px">
+      <Controller
+        render={({ field: { onChange: formOnChange, ...rest } }) => (
+          <Autocomplete
+            {...rest}
+            id="grouped-demo"
+            isOptionEqualToValue={(option, value) =>
+              option.ticketId === value.ticketId
+            }
+            options={data?.data || []}
+            multiple
+            sx={{ width: '100%' }}
+            filterOptions={(x) => x}
+            disableCloseOnSelect
+            onChange={(_ev, newValue) => formOnChange(newValue)}
+            getOptionLabel={(option) => option.description}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                fullWidth
+                title={t('search_secondary_placeholder')}
+                placeholder={t('search_secondary_placeholder')}
+                size="small"
+                autoFocus
+                error={hasError}
+                required={true}
+                InputProps={{
+                  ...params.InputProps,
+                  endAdornment: (
+                    <React.Fragment>
+                      {isLoading ? (
+                        <CircularProgress color="inherit" size={20} />
+                      ) : null}
+                      {params.InputProps.endAdornment}
+                    </React.Fragment>
+                  ),
+                }}
+                onChange={debouncedChange}
+              />
+            )}
+            renderOption={(optionProps, option, { selected }) => {
+              return (
+                <li {...optionProps} key={option.ticketId}>
+                  <TicketInfo
+                    multiSelect
+                    checked={selected}
+                    ticketDetails={{
+                      customerName: option.customerName,
+                      description: option.description,
+                      ticketStatus: option.ticketStatus,
+                      ticketId: option.ticketId,
+                    }}
+                  />
+                </li>
+              );
+            }}
+          />
+        )}
+        control={control}
+        name={'searchTickets'}
+        rules={{ validate: validateField }}
+      />
+      <ErrorMessage
+        errors={errors}
+        name={'searchTickets'}
+        as={StyledErrorMessage}
+      />
+    </FlexBox>
+  );
+};
+
 export const LinkTicketDialog = (props: LinkTicketDialogProps) => {
   const { openLinkTicketDialog, toggleLinkTicketDialog } = props;
-  const { t } = useTranslation();
   const form = useForm<ISearchFormFields>();
   const { mutateAsync, data, isLoading } = useSearchTickets();
   const { mutateAsync: linkTicket, isLoading: mutationLoading } =
@@ -108,97 +199,5 @@ export const LinkTicketDialog = (props: LinkTicketDialogProps) => {
         </DialogActions>
       </Dialog>
     </FormProvider>
-  );
-};
-
-const SearchTickets = (props: {
-  isLoading: boolean;
-  data?: ISearchTickets;
-  onChange: React.ChangeEventHandler<HTMLInputElement>;
-}) => {
-  const { data, isLoading, onChange } = props;
-  const {
-    formState: { errors },
-    control,
-  } = useFormContext();
-  const hasError = get(errors, 'searchTickets') !== undefined;
-
-  const validateField = (val: []) => {
-    if (val.length <= 0) {
-      return 'Please make at least 1 selection to merge tickets';
-    }
-  };
-
-  const debouncedChange = debounce(onChange, 500);
-
-  return (
-    <FlexBox flexDirection="column" gap={'5px'} width="400px">
-      <Controller
-        render={({ field: { onChange: formOnChange, ...rest } }) => (
-          <Autocomplete
-            {...rest}
-            id="grouped-demo"
-            isOptionEqualToValue={(option, value) =>
-              option.ticketId === value.ticketId
-            }
-            options={data?.data || []}
-            multiple
-            sx={{ width: '100%' }}
-            filterOptions={(x) => x}
-            disableCloseOnSelect
-            onChange={(_ev, newValue) => formOnChange(newValue)}
-            getOptionLabel={(option) => option.description}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                fullWidth
-                title="Search a ticket by ID or Subject"
-                placeholder="Search a ticket by ID or Subject"
-                size="small"
-                autoFocus
-                error={hasError}
-                required={true}
-                InputProps={{
-                  ...params.InputProps,
-                  endAdornment: (
-                    <React.Fragment>
-                      {isLoading ? (
-                        <CircularProgress color="inherit" size={20} />
-                      ) : null}
-                      {params.InputProps.endAdornment}
-                    </React.Fragment>
-                  ),
-                }}
-                onChange={debouncedChange}
-              />
-            )}
-            renderOption={(props, option, { selected }) => {
-              return (
-                <li {...props} key={option.ticketId}>
-                  <TicketInfo
-                    multiSelect
-                    checked={selected}
-                    ticketDetails={{
-                      customerName: option.customerName,
-                      description: option.description,
-                      ticketStatus: option.ticketStatus,
-                      ticketId: option.ticketId,
-                    }}
-                  />
-                </li>
-              );
-            }}
-          />
-        )}
-        control={control}
-        name={'searchTickets'}
-        rules={{ validate: validateField }}
-      />
-      <ErrorMessage
-        errors={errors}
-        name={'searchTickets'}
-        as={StyledErrorMessage}
-      />
-    </FlexBox>
   );
 };
