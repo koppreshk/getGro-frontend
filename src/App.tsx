210f738de5a0fecf84fe61df214124d981966a73
koppreshk <koppresh@gmail.com>
2025-01-10 09:56:43 +0530

    fix: added caching for api level

--- src/App.tsx
+++ src/App.tsx
@@ -40,6 +40,8 @@ const queryClient = new QueryClient({
     queries: {
       refetchOnWindowFocus: false,
       retry: false,
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      cacheTime: 10 * 60 * 1000, // 10 minutes
     },
   },
 });
