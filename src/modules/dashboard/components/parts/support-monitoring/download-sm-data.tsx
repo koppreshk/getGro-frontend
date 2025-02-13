48719195354ffd47f0e9c752ae44dff1301cb585
koppreshk <koppresh@gmail.com>
2025-02-13 18:44:16 +0530

    fix: added download sm data

---
+++ src/modules/dashboard/components/parts/support-monitoring/download-sm-data.tsx
@@ -0,0 +1,41 @@
+import { DateRange } from '@matharumanpreet00/react-daterange-picker';
+import { DownloadForOfflineOutlined } from '@mui/icons-material';
+import { t } from 'i18next';
+import { CustomIconButton } from 'lib/ui-ux';
+import { saveAsCSV } from 'lib/utils';
+import { SupportMonitoringValues } from 'modules/dashboard/apis';
+import { DashboardQueryKeys } from 'modules/dashboard/apis/api-enums';
+import { useQueryClient } from 'react-query';
+
+interface DownloadSMDataProps {
+  data: SupportMonitoringValues;
+  dateRange: DateRange;
+}
+
+export const DownloadSMData = (props: DownloadSMDataProps) => {
+  const { data, dateRange } = props;
+  const queryClient = useQueryClient();
+
+  const onDownloadBtnClick = async () => {
+    await queryClient.refetchQueries([
+      DashboardQueryKeys.FETCH_SM_TICKET_STATISTICS,
+      dateRange,
+    ]);
+    const ticketStats = queryClient.getQueryData([
+      DashboardQueryKeys.FETCH_SM_TICKET_STATISTICS,
+      dateRange,
+    ]);
+
+    saveAsCSV([{ ...data, ...(ticketStats as {}) }], {
+      fileName: 'support-monitoring-data.csv',
+    });
+  };
+
+  return (
+    <CustomIconButton
+      iconComponent={<DownloadForOfflineOutlined fontSize="small" />}
+      tooltipProps={{ title: t('download_as_csv') }}
+      onClick={onDownloadBtnClick}
+    />
+  );
+};
